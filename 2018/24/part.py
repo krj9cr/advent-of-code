ced8bbe396a389e4b600252f519f7a6418887222
krj9cr_uva <krj9cr@virginia.edu>
2018-12-24 10:33:25 -0500

    Start on day 24

--- 2018/24/part.py
+++ 2018/24/part.py
@@ -1,11 +1,48 @@
 
 
+class Group:
+    def __init__(self, u=0, h=0, ad=0, at="", i=0):
+        self.units = u
+        self.hp = h
+        self.attack_damage = ad
+        self.attack_type = at
+        self.initiative = i
+        self.weaknesses = set()
+        self.immunities = set()
+        self.effective_power = self.units * self.attack_damage
+
+
 def parseInput(path: str):
+    armies = {}
     with open(path, 'r') as file:
-        return [parseLine(line) for line in file]
+        currentType = ""
+        currentArmy = []
+        for line in file:
+            line = line.strip()
+            if line == "Immune System:":
+                currentType = "immune"
+                currentArmy = []
+                continue
+            elif line == "Infection:":
+                # add immune
+                armies[currentType] = currentArmy
+                currentType = "infection"
+                currentArmy = []
+                continue
+            elif line == "":
+                continue
+            else:
+                currentArmy.append(parseGroup(line))
+        # add infection
+        armies[currentType] = currentArmy
 
 
-def parseLine(line: str):
+def parseGroup(line: str):
+    # 17 units each with
+    # 5390 hit points
+    # (weak to radiation, bludgeoning)
+    # with an attack that does 4507 fire damage
+    # at initiative 2
     return line.strip()
 
 
