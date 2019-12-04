ca6c906db52d1943124dd90e25d096ef94ca3a7f
Karen Craig <karen.craig@ccri.com>
2019-12-04 13:42:25 -0500

    Start on day 4

--- 2019/4/code.py
+++ 2019/4/code.py
@@ -20,43 +20,96 @@ def parseLine(line: str):
 ###########################
 # part1
 ###########################
-def part1(data):
-    print(data)
+def part1():
+    start = 128392
+    end = 643281
+    count = 0
+    for i in range(start,end+1):
+        count += testNum(i)
+    print(count)
 
-def testpart1(data):
-    lines = parseInput(data)
-    part1(lines)
+def testIncrease(s):
+    prev = s[0]
+    sp = s[1:]
+    for char in sp:
+        if int(char) < int(prev):
+            return False
+        else:
+            prev = char
+    return True
+
+def testDouble(s):
+    prev = s[0]
+    sp = s[1:]
+    for char in sp:
+        if prev == char:
+            return True
+        else:
+            prev = char
+    return False
+
+def testNum(i):
+    s = str(i)
+    if len(s) == 6 and testIncrease(s) and testDouble(s):
+        return 1
+    return 0
+
+# 2099 too high
 
 def runpart1():
-    part1(parseInputFile())
+    part1()
 
 ###########################
 # part2
 ###########################
-def part2(data):
-    print(data)
+def testPairs(s):
+    prev = s[0]
+    sp = s[1:]
+    for i in range(0, len(sp)):
+        char = sp[i]
+        if prev == char:
+            if
+        else:
+            prev = char
+    return False
 
-def testpart2(data):
-    lines = parseInput(data)
-    part2(lines)
+def testNum2(i):
+    s = str(i)
+    if len(s) == 6 and testIncrease(s) and testPairs(s) and testDouble(s):
+        return 1
+    return 0
+
+def part2():
+    start = 128392
+    end = 643281
+    count = 0
+    for i in range(start,end+1):
+        count += testNum2(i)
+    print(count)
 
 def runpart2():
-    part2(parseInputFile())
+    part2()
+
+# 1468 too high
+# 1668 too high
 
 ###########################
 # run
 ###########################
 if __name__ == '__main__':
-    print("PART 1 TEST DATA")
-    testpart1("1111")
-    testpart1("1234")
+#     print("PART 1 TEST DATA")
+#     print(testNum(111111))
+#     print(testNum(223450))
+#     print(testNum(123789))
 
-    # print("\nPART 1 RESULT")
-    # runpart1()
+#     print("\nPART 1 RESULT")
+#     runpart1()
 
-    # print("\n\nPART 2 TEST DATA")
-    # testpart2("1122")
-    # testpart2("1111")
+    print("\n\nPART 2 TEST DATA")
+    print(testNum2(122345))
+    print(testNum2(123444))
+    print(testNum2(111122))
 
-    # print("\nPART 2 RESULT")
-    # runpart2()
+
+    print("\nPART 2 RESULT")
+    runpart2()
