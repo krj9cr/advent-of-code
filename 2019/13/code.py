557becf3e6ef8ce2b50bae78a3f95430c95bdeb7
krj9cr_uva <krj9cr@virginia.edu>
2019-12-15 15:37:40 -0500

    Day 13 part 2

--- 2019/13/code.py
+++ 2019/13/code.py
@@ -4,10 +4,6 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0,parentdir)
 from intcode import Intcode
 
-# select a random sample without replacement
-from random import seed
-from random import sample
-
 ###########################
 # helpers
 ###########################
@@ -21,182 +17,6 @@ def parseInput(lines):
 def parseLine(line: str):
     return line.strip()
 
-# def parseOp(op):
-#     s = str(op)
-#     numZerosAdd =  5 - len(s)
-#     zeros = ""
-#     for i in range(0,numZerosAdd):
-#         zeros += "0"
-#     s = zeros + s
-#     modes = s[0:3]
-#     op = s[3:]
-#     return [int(m) for m in reversed(modes)], int(op)
-#
-# def op1(modes, data, i, relativeBase):
-#     arg1 = data[i+1]
-#     arg2 = data[i+2]
-#     mem = data[i+3]
-#     if modes[0] == 0:
-#         arg1 = data[arg1]
-#     elif modes[0] == 2:
-#         arg1 = data[relativeBase+arg1]
-#     if modes[1] == 0:
-#         arg2 = data[arg2]
-#     elif modes[1] == 2:
-#         arg2 = data[relativeBase+arg2]
-#     if modes[2] == 2:
-#         mem += relativeBase
-#     data[mem] = arg1 + arg2
-#
-# def op2(modes, data, i, relativeBase):
-#     arg1 = data[i+1]
-#     arg2 = data[i+2]
-#     mem = data[i+3]
-#     if modes[0] == 0:
-#         arg1 = data[arg1]
-#     elif modes[0] == 2:
-#         arg1 = data[relativeBase+arg1]
-#     if modes[1] == 0:
-#         arg2 = data[arg2]
-#     elif modes[1] == 2:
-#         arg2 = data[relativeBase+arg2]
-#     if modes[2] == 2:
-#         mem += relativeBase
-#     data[mem] = arg1 * arg2
-#
-# def op3(modes, inp, data, i, relativeBase):
-#     arg = data[i+1]
-#     if modes[0] == 0:
-#         arg = data[arg]
-#     elif modes[0] == 2:
-#         arg += relativeBase
-#     data[arg] = inp
-#
-# def op4(modes, data, i, relativeBase):
-#     arg = data[i+1]
-#     if modes[0] == 0:
-#         arg = data[arg]
-#     elif modes[0] == 2:
-#         arg = data[relativeBase+arg]
-#     return arg
-#
-# def op5(modes, data, i, relativeBase):
-#     arg1 = data[i+1]
-#     arg2 = data[i+2]
-#     if modes[0] == 0:
-#         arg1 = data[arg1]
-#     elif modes[0] == 2:
-#         arg1 = data[relativeBase+arg1]
-#     if modes[1] == 0:
-#         arg2 = data[arg2]
-#     elif modes[1] == 2:
-#         arg2 = data[relativeBase+arg2]
-#     if arg1 != 0:
-#         return arg2
-#     else:
-#         return i+3
-#
-# def op6(modes, data, i, relativeBase):
-#     arg1 = data[i+1]
-#     arg2 = data[i+2]
-#     if modes[0] == 0:
-#         arg1 = data[arg1]
-#     elif modes[0] == 2:
-#         arg1 = data[relativeBase+arg1]
-#     if modes[1] == 0:
-#         arg2 = data[arg2]
-#     elif modes[1] == 2:
-#         arg2 = data[relativeBase+arg2]
-#     if arg1 == 0:
-#         return arg2
-#     else:
-#         return i+3
-#
-# def op7(modes, data, i, relativeBase):
-#     arg1 = data[i+1]
-#     arg2 = data[i+2]
-#     mem  = data[i+3]
-#     if modes[0] == 0:
-#         arg1 = data[arg1]
-#     elif modes[0] == 2:
-#         arg1 = data[relativeBase+arg1]
-#     if modes[1] == 0:
-#         arg2 = data[arg2]
-#     elif modes[1] == 2:
-#         arg2 = data[relativeBase+arg2]
-#     if modes[2] == 2:
-#         mem += relativeBase
-#     if arg1 < arg2:
-#         data[mem] = 1
-#     else:
-#         data[mem] = 0
-#
-# def op8(modes, data, i, relativeBase):
-#     arg1 = data[i+1]
-#     arg2 = data[i+2]
-#     mem  = data[i+3]
-#     if modes[0] == 0:
-#         arg1 = data[arg1]
-#     elif modes[0] == 2:
-#         arg1 = data[relativeBase+arg1]
-#     if modes[1] == 0:
-#         arg2 = data[arg2]
-#     elif modes[1] == 2:
-#         arg2 = data[relativeBase+arg2]
-#     if modes[2] == 2:
-#         mem += relativeBase
-#     if arg1 == arg2:
-#         data[mem] = 1
-#     else:
-#         data[mem] = 0
-#
-# def op9(modes, data, i, relativeBase):
-#     arg = data[i+1]
-#     if modes[0] == 0:
-#         arg = data[arg]
-#     elif modes[0] == 2:
-#         arg = data[relativeBase+arg]
-#     return relativeBase + arg
-#
-# def intcode(data, input, relativeBase):
-#     ip = 0
-#     output = []
-#     while ip < len(data):
-#         nextOp = data[ip]
-#         modes, op = parseOp(nextOp)
-#         if op == 1:
-#             op1(modes, data, ip, relativeBase)
-#             ip += 4
-#         elif op == 2:
-#             op2(modes, data, ip, relativeBase)
-#             ip += 4
-#         elif op == 3:
-#             op3(modes, input, data, ip, relativeBase)
-#             ip += 2
-#         elif op == 4:
-#             output.append(op4(modes, data, ip, relativeBase))
-#             ip += 2
-#         elif op == 5:
-#             ip = op5(modes, data, ip, relativeBase)
-#         elif op == 6:
-#             ip = op6(modes, data, ip, relativeBase)
-#         elif op == 7:
-#             op7(modes, data, ip, relativeBase)
-#             ip += 4
-#         elif op == 8:
-#             op8(modes, data, ip, relativeBase)
-#             ip += 4
-#         elif op == 9:
-#             relativeBase = op9(modes, data, ip, relativeBase)
-#             ip += 2
-#         elif op == 99:
-#             break
-#         else:
-#             print("BAD OPCODE:",op)
-#             exit(1)
-#     return output
-
-
 def chunkIt(seq, num):
     avg = len(seq) / float(num)
     out = []
@@ -205,8 +25,8 @@ def chunkIt(seq, num):
     while last < len(seq):
         out.append(seq[int(last):int(last + avg)])
         last += avg
-
     return out
+
 ###########################
 # part1
 ###########################
@@ -235,27 +55,48 @@ def runpart1():
 def part2(data):
     print(data)
     data[0] = 2
-    inputs = [0]
+    inputs = []
     intcoder = Intcode(data, inputs)
     score = 0
-    # seed random number generator
-    seed(1)
     # possible inputs
-    sequence = [0, -1, 1]
+    outputs = []
+    ballPos = 0
+    padPos = 0
     while intcoder.running:
         # check for outputs
         if len(intcoder.output) >= 3:
+            # print("output",intcoder.output)
             o1, o2, o3 = intcoder.output
+            if o3 == 4:
+                ballPos = o1
+                print("ballPos",ballPos,o2)
+                # provide next input
+                nextinput = 0
+                sign = padPos - ballPos
+                if sign > 0:
+                    nextinput = -1
+                elif sign < 0:
+                    nextinput = 1
+                inputs.append(nextinput)
+                print("nextinput",nextinput)
+            if o3 == 3:
+                padPos = o1
+                print("padPos",padPos,o2)
+                # provide next input
+                nextinput = 0
+                sign = padPos - ballPos
+                if sign > 0:
+                    nextinput = -1
+                elif sign < 0:
+                    nextinput = 1
+                inputs.append(nextinput)
+                print("nextinput",nextinput)
             if o1 == -1 and o2 == 0:
                 score = o3
-        nextinput = sample(sequence, 1)
-        inputs.append(nextinput)
+            outputs.append(intcoder.output)
+            intcoder.output = []
+        intcoder.step()
     print("score",score)
-
-
-def testpart2(data):
-    lines = parseInput(data)
-    part2(lines)
 
 def runpart2():
     part2(parseInputFile())
