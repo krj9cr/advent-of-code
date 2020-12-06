bd7bb500981396269563ab604120fa3f2afaa0b1
krj9cr_uva <krj9cr@virginia.edu>
2020-12-06 15:38:48 -0500

    Day 6 and 7

--- 2020/1-7/5/code.py
+++ 2020/1-7/5/code.py
@@ -17,15 +17,50 @@ def parseInput(lines):
 def parseLine(line: str):
     return line.strip()
 
+def seatNumber(line: str):
+    low = 0
+    high = 127
+    row = 0
+    for letter in line[0:6]:
+        mid = (high + low) // 2
+        # print("low", low, "high", high, "mid", mid)
+        if letter == "F":
+            high = mid
+        elif letter == "B":
+            low = mid + 1
+    if line[6] == "F":
+        row = low
+    elif line[6] == "B":
+        row = high
+
+    low = 0
+    high = 7
+    col = 0
+    for letter in line[-3:]:
+        mid = (high + low) // 2
+        # print("low", low, "high", high, "mid", mid)
+        if letter == "L":
+            high = mid
+        elif letter == "R":
+            low = mid + 1
+    if line[-1] == "L":
+        col = low
+    elif line[-1] == "R":
+        col = high
+    # print("row",row,"col",col)
+    return (row * 8) + col
+
 ###########################
 # part1
 ###########################
 def part1(data):
     print(data)
-
-def testpart1(data):
-    lines = parseInput(data)
-    part1(lines)
+    m = 0
+    for line in data:
+        wow = seatNumber(line)
+        if wow > m:
+            m = wow
+    print(m)
 
 def runpart1():
     part1(parseInputFile())
@@ -35,10 +70,14 @@ def runpart1():
 ###########################
 def part2(data):
     print(data)
-
-def testpart2(data):
-    lines = parseInput(data)
-    part2(lines)
+    m = 938
+    seats = []
+    for line in data:
+        seats.append(seatNumber(line))
+    print(sorted(seats))
+    for i in range(seats[0], m+1):
+        if i not in seats:
+            print(i)
 
 def runpart2():
     part2(parseInputFile())
@@ -47,16 +86,9 @@ def runpart2():
 # run
 ###########################
 if __name__ == '__main__':
-    # print("PART 1 TEST DATA")
-    # testpart1("1111")
-    # testpart1("1234")
 
-    print("\nPART 1 RESULT")
-    runpart1()
+    # print("\nPART 1 RESULT")
+    # runpart1()
 
-    # print("\n\nPART 2 TEST DATA")
-    # testpart2("1122")
-    # testpart2("1111")
-
-    # print("\nPART 2 RESULT")
-    # runpart2()
+    print("\nPART 2 RESULT")
+    runpart2()
