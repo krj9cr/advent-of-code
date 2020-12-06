ff4bda03782014b374e24ecdb9e13169de9efb3f
krj9cr_uva <krj9cr@virginia.edu>
2020-12-06 15:38:48 -0500

    Day 6 and 7

--- 2020/1-7/6/code.py
+++ 2020/1-7/6/code.py
@@ -5,7 +5,19 @@
 ###########################
 def parseInputFile():
     with open((__file__.rstrip("code.py") + "input.txt"), 'r') as file:
-        return [parseLine(line) for line in file]
+        # read lines
+        rawlines = [line.strip() for line in file]
+
+        # batch them into lists separated by empty line
+        batchedlines = []
+        currBatch = []
+        for line in rawlines:
+            if line != '':
+                currBatch.append(line)
+            else:
+                batchedlines.append(currBatch)
+                currBatch = []
+        return batchedlines
 
 def parseFile(path: str):
     with open(path, 'r') as file:
@@ -22,6 +34,20 @@ def parseLine(line: str):
 ###########################
 def part1(data):
     print(data)
+    result = 0
+    for group in data:
+        questions = {}
+        if len(group) == 1:
+            result += len(group[0])
+            print(result)
+            continue
+        else:
+            for person in group:
+                for letter in person:
+                    questions[letter] = 1
+            result += len(questions)
+        print(result)
+
 
 def testpart1(data):
     lines = parseInput(data)
@@ -35,6 +61,25 @@ def runpart1():
 ###########################
 def part2(data):
     print(data)
+    result = 0
+    for group in data:
+        questions = {}
+        if len(group) == 1:
+            result += len(group[0])
+            print(result)
+            continue
+        else:
+            for person in group:
+                for letter in person:
+                    if questions.get(letter) is not None:
+                        questions[letter] = questions[letter] + 1
+                    else:
+                        questions[letter] = 1
+            for q in questions:
+                if questions[q] == len(group):
+                    result += 1
+            # print(questions)
+        print(result)
 
 def testpart2(data):
     lines = parseInput(data)
@@ -51,12 +96,12 @@ if __name__ == '__main__':
     # testpart1("1111")
     # testpart1("1234")
 
-    print("\nPART 1 RESULT")
-    runpart1()
+    # print("\nPART 1 RESULT")
+    # runpart1()
 
     # print("\n\nPART 2 TEST DATA")
     # testpart2("1122")
     # testpart2("1111")
 
-    # print("\nPART 2 RESULT")
-    # runpart2()
+    print("\nPART 2 RESULT")
+    runpart2()
