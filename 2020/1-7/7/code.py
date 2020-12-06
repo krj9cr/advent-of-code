59f9e29dc34a6162473b61cc957c8e8ec4520996
krj9cr_uva <krj9cr@virginia.edu>
2020-12-06 15:40:11 -0500

    Prep day 7

--- 2020/1-7/7/code.py
+++ 2020/1-7/7/code.py
@@ -7,13 +7,6 @@ def parseInputFile():
     with open((__file__.rstrip("code.py") + "input.txt"), 'r') as file:
         return [parseLine(line) for line in file]
 
-def parseFile(path: str):
-    with open(path, 'r') as file:
-        return parseInput(file)
-
-def parseInput(lines):
-    return [parseLine(line) for line in lines]
-
 def parseLine(line: str):
     return line.strip()
 
@@ -22,10 +15,6 @@ def parseLine(line: str):
 ###########################
 def part1(data):
     print(data)
-
-def testpart1(data):
-    lines = parseInput(data)
-    part1(lines)
 
 def runpart1():
     part1(parseInputFile())
@@ -36,10 +25,6 @@ def runpart1():
 def part2(data):
     print(data)
 
-def testpart2(data):
-    lines = parseInput(data)
-    part2(lines)
-
 def runpart2():
     part2(parseInputFile())
 
@@ -47,16 +32,9 @@ def runpart2():
 # run
 ###########################
 if __name__ == '__main__':
-    # print("PART 1 TEST DATA")
-    # testpart1("1111")
-    # testpart1("1234")
 
     print("\nPART 1 RESULT")
     runpart1()
-
-    # print("\n\nPART 2 TEST DATA")
-    # testpart2("1122")
-    # testpart2("1111")
 
     # print("\nPART 2 RESULT")
     # runpart2()
