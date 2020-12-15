23c043862f638b4e5fc2fc1c5d82355f41236a58
krj9cr_uva <krj9cr@virginia.edu>
2020-12-14 21:13:31 -0500

    Add next set of days templates

---
+++ 2020/15-21/15/code.py
@@ -0,0 +1,46 @@
+import time
+
+###########################
+# helpers
+###########################
+def parseInputFile():
+    with open((__file__.rstrip("code.py") + "input.txt"), 'r') as file:
+        return [parseLine(line) for line in file]
+
+def parseLine(line: str):
+    return line.strip()
+
+###########################
+# part1
+###########################
+def part1(data):
+    print(data)
+
+def runpart1():
+    start = time.perf_counter()
+    part1(parseInputFile())
+    end = time.perf_counter()
+    print(f"Time: {end-start:0.4f}")
+
+###########################
+# part2
+###########################
+def part2(data):
+    print(data)
+
+def runpart2():
+    start = time.perf_counter()
+    part2(parseInputFile())
+    end = time.perf_counter()
+    print(f"Time: {end-start:0.4f}")
+
+###########################
+# run
+###########################
+if __name__ == '__main__':
+
+    print("\nPART 1 RESULT")
+    runpart1()
+
+    # print("\nPART 2 RESULT")
+    # runpart2()
