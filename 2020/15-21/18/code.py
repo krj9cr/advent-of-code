e68e9de0174b000f316dee9731debd4283bb392c
krj9cr_uva <krj9cr@virginia.edu>
2020-12-19 12:33:38 -0500

    Progress on 18 and 19

--- 2020/15-21/18/code.py
+++ 2020/15-21/18/code.py
@@ -1,4 +1,8 @@
+import sys
 import time
+from anytree import AnyNode, RenderTree, PreOrderIter, PostOrderIter
+from copy import deepcopy
+import ast
 
 ###########################
 # helpers
@@ -10,11 +14,159 @@ def parseInputFile():
 def parseLine(line: str):
     return line.strip()
 
+
+
+class v(ast.NodeVisitor):
+
+    def __init__(self):
+        self.tokens = []
+        self.result = -1
+        self.lastop = None
+
+    def f_continue(self, node):
+        super(v, self).generic_visit(node)
+
+    def visit_Add(self, node):
+        self.tokens.append('+')
+        self.lastop = "+"
+        self.f_continue(node)
+
+    def visit_BinOp(self, node):
+        right = self.visit(node.right).value
+        left = self.visit(node.left).value
+        if type(node.op) is ast.Add:
+            print(left + right)
+        elif type(node.op) is ast.Mult:
+            print(left * right)
+        # self.visit(node.op)
+
+    def visit_Div(self, node):
+        self.tokens.append('/')
+        self.f_continue(node)
+
+    def visit_Expr(self, node):
+        # print('visit_Expr')
+        return self.f_continue(node)
+
+    def visit_Mult(self, node):
+        self.tokens.append('*')
+        self.f_continue(node)
+
+    def visit_Num(self, node):
+        # if self.result == -1:
+        #     self.result = node.n
+        self.tokens.append(node.n)
+        self.f_continue(node)
+        print(node.n)
+
+def idxOfEndParen(equation):
+    stack = []
+    for i in range(len(equation)):
+        char = equation[i]
+        if char == ")":
+            stack.append(char)
+        elif char == "(":
+            stack.pop()
+        if len(stack) == 0:
+            return i
+
+def parseEquation(equation):
+    print(equation)
+    if len(equation) == 0:
+        return None
+    char = equation[0]
+    if len(equation) == 1:
+        if char.isdigit():
+            return AnyNode(type="num", data=int(char))
+        else:
+            print("HUH")
+            sys.exit(1)
+    else:
+        if char.isdigit():
+            num = int(char)
+            op = equation[2]
+            rest = equation[4:]
+            return AnyNode(children=[AnyNode(type="num", data=num), parseEquation(rest)], type="op", data=op)
+        elif char == ")":
+            # find matching end paren
+
+            if equation[-1] == "(":
+                return parseEquation(equation[1:-1])
+            else:
+                idx = -1
+                for i in range(len(equation)-1,1,-1):
+                    if equation[i] == "(":
+                        idx = i
+                        break
+                blob = equation[1:idx]
+                if idx+2 >= len(equation):
+                    print("UH OH")
+                    sys.exit(1)
+                op = equation[idx+2]
+                rest = equation[idx+4:]
+                return AnyNode(children=[parseEquation(blob), parseEquation(rest)], type="op", data=op)
+
+def solveEquation(root):
+    if root.__getattribute__("type") == "num":
+        return root.__getattribute__("data")
+    else:
+        op = root.__getattribute__("data")
+        left = solveEquation(root.children[0])
+        right = solveEquation(root.children[1])
+        if op == "*":
+            return left * right
+        elif op == "+":
+            return left + right
+    print("BADNESS")
+    sys.exit(1)
+
+def rrr(tree):
+    if type(tree) is ast.Num:
+        return tree.n
+    elif type(tree) is ast.BinOp:
+        left = rrr(tree.left)
+        right = rrr(tree.right)
+        op = tree.op
+        if type(op) is ast.Add:
+            return left + right
+        elif type(op) is ast.Mult:
+            return left * right
+    elif type(tree) is ast.Expr:
+        return rrr(tree.value)
+    elif type(tree) is ast.Module:
+        return rrr(tree.body[0])
+
+def doEquation(equation):
+    # parse
+    leaf = parseEquation(equation[::-1])
+    root = deepcopy(leaf)
+    while root.parent is not None:
+        root = root.parent
+    print(RenderTree(root))
+
+    # iter to solve
+    order = [node for node in PreOrderIter(root)]
+    print(order)
+    # solve
+    return solveEquation(root)
+
+    # visitor = v()
+    # tree = ast.parse(equation)
+    # print(ast.dump(tree))
+    # print(rrr(tree))
+    # return 0
+
 ###########################
 # part1
 ###########################
 def part1(data):
     print(data)
+    result = 0
+    for equation in data:
+        val = doEquation(equation)
+        print(equation, "=", val)
+        result += val
+    print("sum", result)
 
 def runpart1():
     start = time.perf_counter()
--- 2020/15-21/19/code.py
+++ 2020/15-21/19/code.py
@@ -1,20 +1,101 @@
 import time
+import sys
 
 ###########################
 # helpers
 ###########################
 def parseInputFile():
     with open((__file__.rstrip("code.py") + "input.txt"), 'r') as file:
-        return [parseLine(line) for line in file]
+        # read lines
+        rawlines = [line.strip() for line in file]
+
+        # batch them into lists separated by empty line
+        batchedlines = []
+        currBatch = []
+        for line in rawlines:
+            if line != '':
+                currBatch.append(line)
+            else:
+                batchedlines.append(currBatch)
+                currBatch = []
+        batchedlines.append(currBatch)
+
+        rawrules = batchedlines[0]
+        messages = batchedlines[1]
+
+        rules = {}
+        for rule in rawrules:
+            r = rule.split(": ")
+            key = int(r[0])
+            val1 = r[1].strip("\"")
+            if val1.isalpha():
+                rules[key] = val1
+            else:
+                vals = []
+                if "|" not in val1:
+                    rules[key] = [int(v) for v in val1.split(" ")]
+                else:
+                    val2 = val1.split("|")
+                    for s in val2:
+                        val3 = s.strip().split(" ")
+                        vals.append([ int(v) for v in val3])
+
+                    rules[key] = tuple(vals)
+
+        return rules, messages
 
 def parseLine(line: str):
     return line.strip()
+
+def match(rules, message, rule):
+    print("evaluating",message,"against",rule)
+    # single character rule
+    if isinstance(rule, str):
+        if message == rule:
+            return True
+        else:
+            return False
+    # just a rule idx
+    elif isinstance(rule, int):
+        return match(rules, message, rules[rule])
+    # no OR in rule
+    elif isinstance(rule, list):
+        m = True
+        div = len(message) // len(rule)
+        # more chars than we can match
+        if div * len(rule) != len(message):
+            return False
+        for i in range(len(rule)):
+            first = i * div
+            last = (i+1)*div
+            print(first, last, message[first:last])
+            m = m and match(rules, message[first:last], rule[i])
+        return m
+    # OR in rule
+    elif isinstance(rule, tuple):
+        m = False
+        for subrule in rule:
+            m = m or match(rules, message, subrule)
+        return m
+    else:
+        print("IDK")
+        sys.exit(1)
 
 ###########################
 # part1
 ###########################
 def part1(data):
     print(data)
+    rules, messages = data
+
+    result = 0
+    for message in messages:
+        if match(rules, message, rules[0]):
+            print("Matches", message)
+            result += 1
+        else:
+            print("No match", message)
+    print("Result: ", result)
 
 def runpart1():
     start = time.perf_counter()
