e9792cc078e278498362e24c47dadf2662f7d943
krj9cr_uva <krj9cr@virginia.edu>
2020-12-24 17:42:22 -0500

    Update linkedlist

--- 2020/22-25/23/code.py
+++ 2020/22-25/23/code.py
@@ -1,7 +1,7 @@
 import time
 from copy import deepcopy
 from collections import deque
-from lib.linkedlist import Node, LinkedList
+from lib.linkedlist import Node, CircularLinkedList
 
 ###########################
 # helpers
@@ -14,65 +14,28 @@ def parseLine(line: str):
     return [ int(s) for s in line.strip() ]
 
 def getLabelsAfterOne(cups):
-    cups = list(cups)
-    idx = cups.index(1)
-    after = cups[idx+1:]
-    before = cups[:idx]
-    l = after + before
-    return "".join([ str(i) for i in l ])
-
-def move(cups, lenCups):
-    minCup = 1
-    maxCup = lenCups
-
-    # print("cups:", cups)
-    currentCup = cups.popleft()
-    pickup = [cups.popleft(), cups.popleft(), cups.popleft()]
-
-    # destination cup: the cup with a label equal to the current cup's label minus one
-    destinationCup = currentCup - 1
-    # If this would select one of the cups that was just picked up,
-    # the crab will keep subtracting one until it finds a cup that wasn't just picked up
-    while destinationCup in pickup:
-        destinationCup -= 1
-    # If at any point in this process the value goes below the lowest value on any cup's label,
-    # it wraps around to the highest value on any cup's label instead
-    if destinationCup < minCup:
-        destinationCup = maxCup
-
-    try:
-        destinationIdx = cups.index(destinationCup)
-    except:
-        destinationCup = max(cups)
-        destinationIdx = cups.index(destinationCup)
-        pass
-
-    for i in range(len(pickup)):
-        cups.insert(destinationIdx+i+1, pickup[i])
-
-    cups.append(currentCup)
-
-    return cups
-
+    oneNode = cups.find(1)
+    cups.head = oneNode
+    l = []
+    for node in cups:
+        l.append(str(node.data))
+    l.pop(0) # remove one
+    return "".join(l)
 
 def move2(cups, lenCups, currentCup):
     minCup = 1
     maxCup = lenCups
 
-    # print("cups:", cups)
+    cups.head = currentCup
+    print("current cup:", currentCup)
+    print("cups:", cups)
     # currentCup = cups.head
     pickup1 = currentCup.next
-    if not pickup1:
-        pickup1 = cups.head
     pickup2 = pickup1.next
-    if not pickup2:
-        pickup2 = cups.head
     pickup3 = pickup2.next
-    if not pickup3:
-        pickup3 = cups.head
 
-    pickupNodes = [pickup1, pickup2, pickup3]
-    pickupData = [pickup1.data, pickup2.data, pickup3.data]
+    pickupData = list(reversed([pickup1.data, pickup2.data, pickup3.data]))
+    print("pickup:", pickupData)
     # destination cup: the cup with a label equal to the current cup's label minus one
     destinationCup = currentCup.data - 1
     # If this would select one of the cups that was just picked up,
@@ -83,14 +46,17 @@ def move2(cups, lenCups, currentCup):
     # it wraps around to the highest value on any cup's label instead
     if destinationCup < minCup:
         destinationCup = maxCup
+    while destinationCup in pickupData:
+        destinationCup -= 1
+    print("desintation:", destinationCup)
 
     # move picked up cups after destination cup
-    for i in range(len(pickupNodes)):
-        cups.remove_node(pickupNodes[i])
-        cups.add_after(destinationCup, pickupNodes[i])
+    for i in range(len(pickupData)):
+        cups.remove_node_with_data(pickupData[i])
+        cups.add_after(destinationCup, Node(pickupData[i]))
 
     destinationNode = cups.find(destinationCup)
-    currentCup = destinationNode.next
+    currentCup = currentCup.next
 
     return cups, currentCup
 
@@ -100,15 +66,15 @@ def move2(cups, lenCups, currentCup):
 def part1(data):
     numCups = len(data)
 
-    cups = LinkedList(data)
+    cups = CircularLinkedList(data)
     print(cups)
 
     currentCup = cups.head
     numMoves = 100
     for i in range(numMoves):
-        # print("move ", i)
+        print("move ", i)
         cups, currentCup = move2(cups, numCups, currentCup)
-        # print()
+        print()
 
     print("Final cups", cups)
     res = getLabelsAfterOne(cups)
--- lib/linkedlist.py
+++ lib/linkedlist.py
@@ -6,9 +6,10 @@ class Node:
         self.next = None
 
     def __repr__(self):
-        return self.data
+        return "Node(" + str(self.data) + ") -> " + str(self.next.data)
 
-class LinkedList:
+# https://www.javatpoint.com/python-program-to-create-and-display-a-circular-linked-list
+class CircularLinkedList:
     def __init__(self, nodes=None):
         self.head = None
         if nodes is not None:
@@ -17,26 +18,35 @@ class LinkedList:
             for elem in nodes:
                 node.next = Node(data=elem)
                 node = node.next
+            node.next = self.head
 
-    def add_first(self, node):
-        node.next = self.head
-        self.head = node
+    # This function will add the new node at the end of the list.
+    def add(self, data):
+        newNode = Node(data)
+        # Checks if the list is empty.
+        if self.head is None:
+            # If list is empty, both head and tail would point to new node.
+            self.head = newNode
+            newNode.next = self.head
+        else:
+            node = None
+            for n in self:
+                node = n
+            node.next = newNode
+            newNode.next = self.head
 
-    def add_last(self, node):
-        if not self.head:
-            self.head = node
-            return
-        for current_node in self:
-            pass
-        current_node.next = node
-
-    def find(self, target_node_data):
+    # Displays all the nodes in the list
+    def display(self):
+        current = self.head
         if not self.head:
             raise Exception("List is empty")
-        for node in self:
-            if node.data == target_node_data:
-                return node
-        raise Exception("Node with data '%s' not found" % target_node_data)
+        else:
+            print("Nodes of the circular linked list: ")
+            #Prints each node by incrementing pointer.
+            print(current.data),
+            while current.next != self.head:
+                current = current.next
+                print(current.data),
 
     def add_after(self, target_node_data, new_node):
         if not self.head:
@@ -48,14 +58,39 @@ class LinkedList:
                 return
         raise Exception("Node with data '%s' not found" % str(target_node_data))
 
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+            if node == self.head:
+                break
+
+    def __repr__(self):
+        nodes = []
+        for node in self:
+            nodes.append(str(node.data))
+        return " -> ".join(nodes)
+
+    def find(self, target_node_data):
+        if not self.head:
+            raise Exception("List is empty")
+        for node in self:
+            if node.data == target_node_data:
+                return node
+        raise Exception("Node with data '%s' not found" % target_node_data)
+
     def remove_node_with_data(self, target_node_data):
         if not self.head:
             raise Exception("List is empty")
 
         if self.head.data == target_node_data:
-            res = deepcopy(self.head)
+            # find whatever was pointing to head :/
+            for node in self:
+                if node.next == self.head:
+                    node.next = self.head.next
+                    break
             self.head = self.head.next
-            return res
 
         previous_node = self.head
         for node in self:
@@ -67,32 +102,12 @@ class LinkedList:
 
         raise Exception("Node with data '%s' not found" % str(target_node_data))
 
-    def remove_node(self, target_node):
-        if not self.head:
-            raise Exception("List is empty")
-
-        if self.head.data == target_node.data:
-            self.head = self.head.next
-
-        previous_node = self.head
-        for node in self:
-            if node.data == target_node.data:
-                previous_node.next = node.next
-            previous_node = node
-
-        raise Exception("Node with data '%s' not found" % str(target_node.data))
-
-    def __iter__(self):
-        node = self.head
-        while node is not None:
-            yield node
-            node = node.next
-
-    def __repr__(self):
-        node = self.head
-        nodes = []
-        while node is not None:
-            nodes.append(str(node.data))
-            node = node.next
-        nodes.append("None")
-        return " -> ".join(nodes)
+if __name__ == '__main__':
+    test = CircularLinkedList([1,2,3])
+    test.add(4)
+    test.add_after(4,Node(10))
+    print(test)
+    test.remove_node_with_data(3)
+    print(test)
+    three = test.find(10)
+    print(three)
