f128f6bc9857ee795c262440d8ed14b19cc74874
krj9cr_uva <krj9cr@virginia.edu>
2020-12-25 13:07:11 -0500

    day 25

--- 2020/22-25/25/code.py
+++ 2020/22-25/25/code.py
@@ -8,13 +8,37 @@ def parseInputFile():
         return [parseLine(line) for line in file]
 
 def parseLine(line: str):
-    return line.strip()
+    return int(line.strip())
+
+def reverse_transform(subject, target):
+    val = 1
+    loopsize = 0
+    while val != target:
+        val *= subject
+        val %= 20201227
+        loopsize += 1
+    return loopsize
+
+def transform(subject, loopsize):
+    val = 1
+    for l in range(loopsize):
+        val *= subject
+        val %= 20201227
+    return val
 
 ###########################
 # part1
 ###########################
 def part1(data):
     print(data)
+    card_public = data[0]
+    door_public = data[1]
+
+    card_loop_size = reverse_transform(7, card_public)
+    door_loop_size = reverse_transform(7, door_public)
+
+    encryption_key = transform(door_public, card_loop_size)
+    print("res", encryption_key)
 
 def runpart1():
     start = time.perf_counter()
