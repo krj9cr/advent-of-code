dfb90313322bd1767f8bd42e3a144d89d81aaa92
krj9cr_uva <krj9cr@virginia.edu>
2020-12-27 15:41:47 -0500

    Optimize day 14

--- year_2020/day_14/code.py
+++ year_2020/day_14/code.py
@@ -1,5 +1,4 @@
 import time
-from copy import deepcopy
 
 ###########################
 # helpers
@@ -9,47 +8,52 @@ def parseInputFile():
         return [parseLine(line) for line in file]
 
 def parseLine(line: str):
-    return line.strip().split(" = ")
+    l = line.strip().split(" = ")
+    cmd = l[0]
+    val = l[1]
+    if cmd == "mask":
+        return cmd, val
+    elif "mem" in cmd:
+        memloc = int(cmd.replace("mem[", "").replace("]", ""))
+        val = int(val)
+        return "mem", memloc, val
 
 mem = {}
 
 # convert int to array of bits
 def bitfield(n, numbits):
-    a = [1 if digit=='1' else 0 for digit in bin(n)[2:]]
-    return ([0] * (numbits - len(a))) + a
+    a = "{0:b}".format(n)
+    return ("0" * (numbits - len(a))) + a
 
 # apply bit mask to bit array
 # assumes they are the same length
-def applyBitmask(bitmask, bitarr):
-    res = []
+def applyBitmask(bitmask, bitstr):
+    res = ""
     for i in range(len(bitmask)):
         m = bitmask[i]
-        b = bitarr[i]
+        b = bitstr[i]
         if m == "X":
-            res.append(b)
+            res += (b)
         else:
-            res.append(int(m))
+            res += (m)
     return res
 
-def shifting(bitlist):
-    out = 0
-    for bit in bitlist:
-        out = (out << 1) | bit
-    return out
+def shifting(bitstr):
+    return int(bitstr, 2)
 
 def processLine(mask, line):
     cmd = line[0]
     val = line[1]
     if cmd == "mask":
         mask = val
-    elif "mem" in cmd:
-        memloc = int(cmd.replace("mem[", "").replace("]", ""))
-        val = int(val)
+    elif cmd == "mem":
+        memloc = line[1]
+        val = line[2]
         valbits = bitfield(val, len(mask))
-        print(''.join([str(i) for i in valbits]))
-        print(mask)
+        # print(''.join([str(i) for i in valbits]))
+        # print(mask)
         valmasked = applyBitmask(mask, valbits)
-        print(''.join([str(i) for i in valmasked]))
+        # print(''.join([str(i) for i in valmasked]))
         valint = shifting(valmasked)
         mem[memloc] = valint
     return mask
@@ -59,15 +63,14 @@ def processLine(mask, line):
 # part1
 ###########################
 def part1(data):
-    print(data)
-    numbits = 36
+    # print(data)
     mask = "X" * 36
 
     # proess each line
     for line in data:
         mask = processLine(mask, line)
-        print(mem)
-        print()
+        # print(mem)
+        # print()
 
     # result
     prod = 0
@@ -86,16 +89,16 @@ def runpart1():
 # part2
 ###########################
 def applyBitmask2(bitmask, bitarr):
-    res = []
+    res = ""
     for i in range(len(bitmask)):
         m = bitmask[i]
         b = bitarr[i]
         if m == "0":
-            res.append(b)
+            res += b
         elif m == "1":
-            res.append(1)
+            res += "1"
         elif m == "X":
-            res.append("X")
+            res += "X"
         else:
             print("BADNESS")
             return
@@ -107,10 +110,12 @@ def generateAddresses(bitarr):
     for i in range(len(bitarr)):
         b = bitarr[i]
         if b == "X":
-            newbitarr = deepcopy(bitarr)
-            newbitarr[i] = 0
-            newbitarr2 = deepcopy(bitarr)
-            newbitarr2[i] = 1
+            # newbitarr = bitarr[:]
+            # newbitarr[i] = 0
+            newbitarr = bitarr[:i] + ["0"] + bitarr[i+1:]
+            newbitarr2 = bitarr[:i] + ["1"] + bitarr[i+1:]
+            # newbitarr2 = bitarr[:]
+            # newbitarr2[i] = 1
             return generateAddresses(newbitarr) + generateAddresses(newbitarr2)
 
 def processLine2(mask, line):
@@ -118,39 +123,38 @@ def processLine2(mask, line):
     val = line[1]
     if cmd == "mask":
         mask = val
-    elif "mem" in cmd:
-        memloc = int(cmd.replace("mem[", "").replace("]", ""))
-        val = int(val)
+    elif cmd == "mem":
+        memloc = line[1]
+        val = line[2]
         valbits = bitfield(val, len(mask))
-        print(''.join([str(i) for i in valbits]))
-        print(mask)
+        # print(''.join([str(i) for i in valbits]))
+        # print(mask)
         valmasked = applyBitmask(mask, valbits)
-        print(''.join([str(i) for i in valmasked]))
+        # print(''.join([str(i) for i in valmasked]))
         valint = shifting(valmasked)
         # mask memory, too
         memlocmasked = applyBitmask2(mask, bitfield(memloc, len(mask)))
-        addrmasks = generateAddresses(memlocmasked)
+        addrmasks = generateAddresses([c for c in memlocmasked])
         for bitaddr in addrmasks:
-            addr = shifting(bitaddr)
+            addr = shifting("".join([str(b) for b in bitaddr]))
             mem[addr] = val
     return mask
 
 def part2(data):
-    print(data)
+    # print(data)
     mask = "X" * 36
 
     # proess each line
     for line in data:
         mask = processLine2(mask, line)
         # print(mem)
-        print()
+        # print()
 
     # result
-    prod = 0
+    res = 0
     for memloc in mem:
-        val = mem[memloc]
-        prod += val
-    print("DONE", prod)
+        res += mem[memloc]
+    print("DONE", res)
 
 def runpart2():
     start = time.perf_counter()
@@ -163,9 +167,10 @@ def runpart2():
 ###########################
 if __name__ == '__main__':
 
-    # print("\nPART 1 RESULT")
-    # runpart1()
+    print("\nPART 1 RESULT")
+    runpart1()
+
+    mem = {}
 
     print("\nPART 2 RESULT")
     runpart2()
-
