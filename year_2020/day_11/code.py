5cb3459450ff9eacae1af5b76af16f61d5c99ec8
krj9cr_uva <krj9cr@virginia.edu>
2020-12-27 21:37:05 -0500

    Optimize day 11 part 2

--- year_2020/day_11/code.py
+++ year_2020/day_11/code.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 import numpy as np
+from lib.print import print_2d_grid
 import time
 
 ###########################
@@ -18,40 +19,40 @@ def countAdjSet(occupied, i , j):
 
 def doRound(data, occupied, rows, cols):
     newdata = [[ item for item in row ] for row in data]
-    for i in range(rows):
-        row = data[i]
-        newrow = newdata[i]
-        for j in range(cols):
-            item = row[j]
+    for j in range(rows):
+        row = data[j]
+        newrow = newdata[j]
+        for i in range(cols):
+            item = row[i]
             if item == ".": # quicker out
                 continue
             numAdj = countAdjSet(occupied, i, j)
             if item == "L":
                 if numAdj == 0:
-                   newrow[j] = "#"
+                   newrow[i] = "#"
             elif item == "#":
                 if numAdj >= 4:
-                    newrow[j] = "L"
+                    newrow[i] = "L"
     return newdata
 
 # assumes they are the same size
 def compareGrids(data1, data2, rows, cols):
-    for i in range(rows):
-        row = data1[i]
-        row2 = data2[i]
-        for j in range(cols):
-            item = row[j]
-            item2 = row2[j]
+    for j in range(rows):
+        row = data1[j]
+        row2 = data2[j]
+        for i in range(cols):
+            item = row[i]
+            item2 = row2[i]
             if item != item2:
                 return False
     return True
 
 def getOccupied(data, rows, cols):
     occupied = set()
-    for i in range(rows):
-        row = data[i]
-        for j in range(cols):
-            item = row[j]
+    for j in range(rows):
+        row = data[j]
+        for i in range(cols):
+            item = row[i]
             if item == "#":
                 occupied.add(tuple([i,j]))
     return occupied
@@ -85,113 +86,66 @@ def runpart1():
 # part2
 ###########################
 
-def countVisible(data, i, j):
-    return numAsteriodsDetected(data, i, j)
-
-def numAsteriodsDetected(data, x, y):
-    h = len(data[0])
-    w = len(data)
+def countVisible(data, x, y, h, w):
     count = 0
-    # straight directions
-    if y-1 >= 0:
-        j = np.linspace(y,0,y+1)
-        i = 0*j+x
-        count += goDirection(data, i, j, x, y) # up
-    if y+1 < h:
-        j = np.linspace(y, h-1, h-y)
-        i = 0*j+x
-        count += goDirection(data, i, j, x, y) # down
-    if x-1 >= 0:
-        i = np.linspace(x,0,x+1)
-        j = 0*i+y
-        count += goDirection(data, i, j, x, y) #left
-    if x+1 < w:
-        i = np.linspace(x,w-1, w-x)
-        j = 0*i+y
-        count += goDirection(data, i, j, x, y) # right
+    # down
+    count += goDirectionSet(data, [(x, y2) for y2 in range(y + 1, h, 1)])
+    # up
+    count += goDirectionSet(data, [(x, y2) for y2 in range(y - 1, -1, -1)])
+    # left
+    count += goDirectionSet(data, [(x2, y) for x2 in range(x - 1, -1, -1)])
+    # right
+    count += goDirectionSet(data, [(x2, y) for x2 in range(x + 1, w, 1)])
+    # down right
+    count += goDirectionSet(data, [(x2, y2) for (x2, y2) in zip(range(x + 1, w, 1), range(y + 1, h, 1))])
+    # up right
+    count += goDirectionSet(data, [(x2, y2) for (x2, y2) in zip(range(x - 1, -1, -1), range(y + 1, h, 1))])
+    # down left
+    count += goDirectionSet(data, [(x2, y2) for (x2, y2) in zip(range(x + 1, w, 1), range(y - 1, -1, -1))])
+    # down left
+    count += goDirectionSet(data, [(x2, y2) for (x2, y2) in zip(range(x - 1, -1, -1), range(y - 1, -1, -1))])
 
-    # main diagonals
-    if y-1 >= 0 and x-1 >= 0:
-        j = np.linspace(y,0,y+1)
-        i = np.linspace(x,0,x+1)
-        slopes = []
-        for yslope in range(1, 2):
-            for xslope in range(1, 2):
-                slopes.append((xslope,yslope))
-        for (xslope, yslope) in slopes:
-            ys = j[::yslope]
-            xs = i[::xslope]
-            count += goDirection(data, xs, ys, x, y) # up left
-    if y-1 >= 0 and x+1 < w:
-        j = np.linspace(y,0,y+1)
-        i = np.linspace(x, w-1, w-x)
-        for yslope in range(1, 2):
-            ys = j[::yslope]
-            for xslope in range(1, 2):
-                xs = i[::xslope]
-                count += goDirection(data, xs, ys, x, y) # up right
-    if y+1 < h and x-1 >= 0:
-        j = np.linspace(y, h-1, h-y)
-        i = np.linspace(x,0,x+1)
-        for yslope in range(1, 2):
-            ys = j[::yslope]
-            for xslope in range(1, 2):
-                xs = i[::xslope]
-                count += goDirection(data, xs, ys, x, y) # down left
-    if y+1 < h and x+1 < w:
-        j = np.linspace(y, h-1, h-y)
-        i = np.linspace(x, w-1, w-x)
-        for yslope in range(1, 2):
-            ys = j[::yslope]
-            for xslope in range(1, 2):
-                xs = i[::xslope]
-                count += goDirection(data, xs, ys, x, y) # down right
     return count
 
-def goDirection(data, x, y, startx, starty):
-    points = [(int(i),int(j)) for (i,j) in list(zip(x,y))]
-    # print(points)
+def goDirectionSet(data, points):
     for (i, j) in points:
-        if (i,j) != (startx,starty):
-            if 0 <= i < len(data) and 0 <= j < len(data[0]):
-                if data[i][j] == '#':
-                    return 1
-                elif data[i][j] == "L":
-                    return 0
+        if data[j][i] == '#':
+            return 1
+        elif data[j][i] == "L":
+            return 0
     return 0
 
-def doRound2(data):
-    newdata = deepcopy(data)
-    for i in range(len(data)):
-        row = data[i]
-        for j in range(len(row)):
-            # print(i,j)
-            item = row[j]
+def doRound2(data, rows, cols):
+    newdata = [[ item for item in row ] for row in data]
+    for j in range(rows):
+        row = data[j]
+        newrow = newdata[j]
+        for i in range(cols):
+            item = row[i]
+            if item == ".": # quicker out
+                continue
+            numVisible = countVisible(data, i, j, rows, cols)
             if item == "L":
-                if countVisible(data, i, j) == 0:
-                    newdata[i][j] = "#"
+                if numVisible == 0:
+                    newrow[i] = "#"
             elif item == "#":
-                if countVisible(data, i, j) >= 5:
-                    newdata[i][j] = "L"
+                if numVisible >= 5:
+                    newrow[i] = "L"
     return newdata
 
-
 def part2(data):
-    # print_2d_grid(data)
-    prev = data[:]
+    prev = data
+    rows = len(data)
+    cols = len(data[0])
     i = 0
-    # while i < 1:
     while True:
-        print("Round", i)
-        newdata = doRound2(prev)
-        # print_2d_grid(newdata)
-        if compareGrids(prev, newdata):
-            result = countOccupied(newdata)
-            print(result)
+        # print("Round", i)
+        newdata = doRound2(prev, rows, cols)
+        if compareGrids(prev, newdata, rows, cols):
+            print(len(getOccupied(newdata, rows, cols)))
             return
-        prev = newdata[:]
+        prev = newdata
         i +=1
-    # print(len(data[0]),len(data))
 
 def runpart2():
     start = time.perf_counter()
@@ -204,8 +158,8 @@ def runpart2():
 ###########################
 if __name__ == '__main__':
 
-    print("\nPART 1 RESULT")
-    runpart1()
+    # print("\nPART 1 RESULT")
+    # runpart1()
 
-    # print("\nPART 2 RESULT")
-    # runpart2()
+    print("\nPART 2 RESULT")
+    runpart2()
