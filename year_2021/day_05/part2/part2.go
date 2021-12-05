97d9cee22c50b88ebbc515a3de02dcd6fcb5b986
krj9cr_uva <krj9cr@virginia.edu>
2021-12-05 13:34:19 -0500

    Day 4 and 5

---
+++ year_2021/day_05/part2/part2.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	day05 "year_2021/day_05"
+)
+
+func main() {
+
+	if len(os.Args) < 2 {
+		fmt.Println("Missing param, provide input file path")
+		return
+	}
+	lines := day05.ReadInput(os.Args[1])
+	fmt.Printf("Input: %v\n", lines)
+
+	_, maxx, _, maxy := day05.FindMinMaxCoords(lines)
+
+	fmt.Printf("maxx: %v, maxy: %v", maxx, maxy)
+	grid := make([][]int, maxx+2)
+	for i := range grid {
+		grid[i] = make([]int, maxy+2)
+	}
+
+	// utils.PrintGrid(grid)
+
+	for _, line := range lines {
+		line.Chart(grid, true)
+	}
+	// utils.PrintGrid(grid)
+
+	count := 0
+	for _, row := range grid {
+		for _, item := range row {
+			if item > 1 {
+				count += 1
+			}
+		}
+	}
+
+	fmt.Printf("Result: %v\n", count)
+
+	//15413 too high
+	//15284 too high
+}
--- year_2021/go.mod
+++ year_2021/go.mod
@@ -2,8 +2,10 @@ module year_2021
 
 go 1.17
 
+require github.com/fatih/color v1.13.0
+
 require (
-	github.com/fatih/color v1.13.0
+	github.com/MindorksOpenSource/gogeom v0.0.0-20190206120254-bbbf10fd0352 // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
