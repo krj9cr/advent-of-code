2579fcd8d77a3333dae8e075f31be1e4631e72af
krj9cr_uva <krj9cr@virginia.edu>
2021-12-13 09:24:15 -0500

    Day 13

--- year_2021/day_13/part2/part2.go
+++ year_2021/day_13/part2/part2.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"os"
 	day13 "year_2021/day_13"
+	"year_2021/utils"
 )
 
+// this is really part 1
 func main() {
 
 	if len(os.Args) < 2 {
@@ -15,7 +17,42 @@ func main() {
 	lines := day13.ReadInput(os.Args[1])
 	fmt.Printf("Input: %v\n", lines)
 
-	// DO STUFF
+	// Convert to map to remove duplicates
+	currCoords := make(map[day13.Coord]bool)
+	for _, coord := range lines.Coords {
+		currCoords[coord] = true
+	}
+	// Print
+	keys := make([]day13.Coord, len(currCoords))
+	i := 0
+	for k := range currCoords {
+		keys[i] = k
+		i++
+	}
+	utils.PrintStringGrid(day13.PlotDots(keys), "")
 
-	// fmt.Printf("Result: %v\n", result)
+	// For THE FIRST fold
+	fold := lines.Folds[0]
+	newCoords := make(map[day13.Coord]bool)
+	// For each point
+	for coord, _ := range currCoords {
+		// Reflect the point over the line
+		newCoords[day13.Reflect(coord, fold)] = true
+	}
+	currCoords = newCoords
+	fmt.Printf("currCoords: %v\n", currCoords)
+	// Print grid
+	keys = make([]day13.Coord, len(currCoords))
+	i = 0
+	for k := range currCoords {
+		keys[i] = k
+		i++
+	}
+	utils.PrintStringGrid(day13.PlotDots(keys), "")
+	fmt.Printf("Visible dots: %v\n", len(currCoords))
+
+	// Part 2
+	// HKUJGAJZ lmao
+	// Count remaining points
+	fmt.Printf("Result: %v\n", len(currCoords))
 }
