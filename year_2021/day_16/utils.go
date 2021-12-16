14bca6307fed0e99d626f26469f886eff9ee06f4
krj9cr_uva <krj9cr@virginia.edu>
2021-12-16 09:15:57 -0500

    Start 16

--- year_2021/day_16/utils.go
+++ year_2021/day_16/utils.go
@@ -2,10 +2,13 @@ package day16
 
 import (
 	"bufio"
+	"fmt"
+	"log"
 	"os"
+	"strconv"
 )
 
-func ReadInput(path string) []string {
+func ReadInput(path string) string {
 	file, err := os.Open(path)
 	if err != nil {
 		panic(err)
@@ -21,5 +24,85 @@ func ReadInput(path string) []string {
 		panic(scanner.Err())
 	}
 
-	return lines
+	return lines[0]
+}
+
+func BinaryStringToInt(binary string) int {
+	intval, err := strconv.ParseInt(binary, 2, 64)
+	if err != nil {
+		panic(err)
+	}
+	return int(intval)
+}
+
+func GetBits(binary string, numBits int) (string, int) {
+	typeBits := binary[0:numBits]
+	return binary[numBits:], BinaryStringToInt(typeBits)
+}
+
+func GetVersionAndType(binary string) (string, int, int) {
+	binary, version := GetBits(binary, 3)
+	binary, t := GetBits(binary, 3)
+	return binary, version, t
+}
+
+// returns: binary, result, version
+func HandlePacket(binary string) (string, int, int) {
+	binary, version, t := GetVersionAndType(binary)
+	if t == 4 {
+		binary, literal := HandleLiteral(binary)
+		return binary, literal, version
+	} else {
+		binary, result, versionSum := HandleOperator(binary)
+		return binary, result, version + versionSum
+	}
+}
+
+// return: binary, res, version
+func HandleOperator(binary string) (string, int, int) {
+	lengthTypeId := binary[0]
+	binary = binary[1:]
+	if lengthTypeId == '0' { // 15 bit mode
+		binary, totalLengthBits := GetBits(binary, 15)
+		subPackets := binary[:totalLengthBits]
+		binary = binary[totalLengthBits:]
+		var literals []int
+		versionSum := 0
+		for {
+			if len(subPackets) > 0 {
+				sp, literal, version := HandlePacket(subPackets)
+				versionSum += version
+				literals = append(literals, literal)
+				subPackets = sp
+			} else {
+				break
+			}
+		}
+		// TODO Some operation over literals?
+		fmt.Printf("15-bit literals: %v\n", literals)
+		return binary, 0, versionSum
+	} else if lengthTypeId == '1' { //  11 bit mode
+
+	} else {
+		log.Panicf("unknown lengthTypeId %v\n", lengthTypeId)
+	}
+	return binary, 0, 0
+}
+
+func HandleLiteral(binary string) (string, int) {
+	numBinary := ""
+	// Parse groups of packets until one starts with '0'
+	for {
+		// Get next 5
+		five := binary[0:5]
+		binary = binary[5:]
+		fmt.Printf("Next five: %v\n", five)
+		numBinary += five[1:]
+		if five[0] == '1' {
+			continue
+		} else { // last packet
+			break
+		}
+	}
+	return binary, BinaryStringToInt(numBinary)
 }
