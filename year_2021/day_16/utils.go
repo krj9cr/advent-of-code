4ecb82575c7e6bfac2174db7e1ff5b10f53f30a3
krj9cr_uva <krj9cr@virginia.edu>
2021-12-16 21:03:10 -0500

    Day 16 pt 2

--- year_2021/day_16/utils.go
+++ year_2021/day_16/utils.go
@@ -53,13 +53,80 @@ func HandlePacket(binary string) (string, int, int) {
 		binary, literal := HandleLiteral(binary)
 		return binary, literal, version
 	} else {
-		binary, result, versionSum := HandleOperator(binary)
+		binary, result, versionSum := HandleOperator(binary, t)
 		return binary, result, version + versionSum
 	}
 }
 
+func EvalOperator(op int, literals []int) int {
+	if op == 0 { // sum
+		sum := 0
+		for _, l := range literals {
+			sum += l
+		}
+		return sum
+	} else if op == 1 { // product
+		prod := 1
+		for _, l := range literals {
+			prod *= l
+		}
+		return prod
+	} else if op == 2 { // min
+		min := 999999999
+		for _, l := range literals {
+			if l < min {
+				min = l
+			}
+		}
+		return min
+	} else if op == 3 { // max
+		max := -9999999
+		for _, l := range literals {
+			if l > max {
+				max = l
+			}
+		}
+		return max
+	} else if op == 4 {
+		log.Panicf("trying to evaluate literal\n")
+	} else if op == 5 { // greater than
+		if len(literals) != 2 {
+			log.Panicf("greater than op is missing sub packets\n")
+		}
+		a := literals[0]
+		b := literals[1]
+		if a > b {
+			return 1
+		}
+		return 0
+	} else if op == 6 { // less than
+		if len(literals) != 2 {
+			log.Panicf("less than op is missing sub packets\n")
+		}
+		a := literals[0]
+		b := literals[1]
+		if a < b {
+			return 1
+		}
+		return 0
+	} else if op == 7 { // equal
+		if len(literals) != 2 {
+			log.Panicf("equals op is missing sub packets\n")
+		}
+		a := literals[0]
+		b := literals[1]
+		if a == b {
+			return 1
+		}
+		return 0
+	} else {
+		log.Panicf("unknown op: %v", op)
+	}
+	return 0
+}
+
 // return: binary, res, version
-func HandleOperator(binary string) (string, int, int) {
+func HandleOperator(binary string, op int) (string, int, int) {
 	lengthTypeId := binary[0]
 	binary = binary[1:]
 	if lengthTypeId == '0' { // 15 bit mode
@@ -78,11 +145,20 @@ func HandleOperator(binary string) (string, int, int) {
 				break
 			}
 		}
-		// TODO Some operation over literals?
 		fmt.Printf("15-bit literals: %v\n", literals)
-		return binary, 0, versionSum
+		return binary, EvalOperator(op, literals), versionSum
 	} else if lengthTypeId == '1' { //  11 bit mode
-
+		binary, numSubPackets := GetBits(binary, 11)
+		var literals []int
+		versionSum := 0
+		for i := 0; i < numSubPackets; i++ {
+			b, literal, version := HandlePacket(binary)
+			binary = b
+			versionSum += version
+			literals = append(literals, literal)
+		}
+		fmt.Printf("11-bit literals: %v\n", literals)
+		return binary, EvalOperator(op, literals), versionSum
 	} else {
 		log.Panicf("unknown lengthTypeId %v\n", lengthTypeId)
 	}
