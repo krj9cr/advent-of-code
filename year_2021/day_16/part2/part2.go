81225721da01b78e7e424cfb6545b451af27f244
krj9cr_uva <krj9cr@virginia.edu>
2021-12-16 21:03:10 -0500

    Day 16 pt 2

--- year_2021/day_16/part2/part2.go
+++ year_2021/day_16/part2/part2.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/hex"
 	"fmt"
 	"os"
 	day16 "year_2021/day_16"
@@ -12,10 +13,22 @@ func main() {
 		fmt.Println("Missing param, provide input file path")
 		return
 	}
-	lines := day16.ReadInput(os.Args[1])
-	fmt.Printf("Input: %v\n", lines)
+	line := day16.ReadInput(os.Args[1])
+	fmt.Printf("Input: %v\n", line)
 
-	// DO STUFF
+	// Convert hex to binary string
+	hexBytes, err := hex.DecodeString(line)
+	if err != nil {
+		panic(err)
+	}
+	binary := ""
+	for _, n := range hexBytes {
+		binary += fmt.Sprintf("%08b", n)
+	}
 
-	// fmt.Printf("Result: %v\n", result)
+	fmt.Printf("Binary: %v\n", binary)
+
+	_, res, _ := day16.HandlePacket(binary)
+	fmt.Printf("result: %v\n", res)
+	// fmt.Printf("version sum: %v\n", versionSum)
 }
