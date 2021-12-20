10e709b01a49adec90071b0980f5a264df8cd90b
krj9cr_uva <krj9cr@virginia.edu>
2021-12-20 12:25:11 -0500

    Day 20

--- year_2021/day_20/part1/part1.go
+++ year_2021/day_20/part1/part1.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 	day20 "year_2021/day_20"
+	"year_2021/utils"
 )
 
 func main() {
@@ -12,10 +13,59 @@ func main() {
 		fmt.Println("Missing param, provide input file path")
 		return
 	}
-	lines := day20.ReadInput(os.Args[1])
-	fmt.Printf("Input: %v\n", lines)
+	enhancement, image := day20.ReadInput(os.Args[1])
+	fmt.Printf("Enhancement: \n%v\n\nImage:\n", enhancement)
+	utils.PrintStringSlice(image)
 
-	// DO STUFF
+	steps := 2
 
-	// fmt.Printf("Result: %v\n", result)
+	// For num steps
+	for step := 0; step < steps; step++ {
+		fmt.Printf("Step: %v\n", step)
+		// Get assumed empty char, since the infinite image area
+		// osciallates between black and white pixels based on the enhance algorithm
+		empty := "."
+		if step > 0 {
+			if step%2 == 0 { // even
+				if enhancement[0] == '.' {
+					empty = "."
+				} else {
+					empty = "#"
+				}
+			} else { //odd
+				if enhancement[len(enhancement)-1] == '#' {
+					empty = "."
+				} else {
+					empty = "#"
+				}
+			}
+		}
+		// Add 2 rows padding around image
+		image2 := day20.PadImage(image, 1, empty)
+		image = image2
+		utils.PrintStringSlice(image)
+		fmt.Print("\n")
+
+		// Make a copy of image for us to update and simultaneously consider pixels
+		var imageCopy []string
+		// For each pixel (not considering outer most padding?)
+		for j := 0; j < len(image); j++ {
+			row := []byte(image[j])
+
+			for i := 0; i < len(row); i++ {
+				// Get the 9 pixels around it, convert them to binary, then decimal
+				newPixel := day20.GetNewPixel(enhancement, image, i, j, empty)
+				// Get the pixel from enhancement and set the center pixel
+				row[i] = newPixel
+			}
+			imageCopy = append(imageCopy, string(row))
+		}
+		// utils.PrintStringSlice(image)
+		image = imageCopy
+		utils.PrintStringSlice(image)
+		fmt.Print("\n")
+	}
+
+	count := day20.NumLitPixels(image)
+	fmt.Printf("Result: %v\n", count)
 }
--- year_2021/day_20/utils.go
+++ year_2021/day_20/utils.go
@@ -2,10 +2,136 @@ package day20
 
 import (
 	"bufio"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func ReadInput(path string) []string {
+func NumLitPixels(image []string) int {
+	count := 0
+	for _, row := range image {
+		for _, item := range row {
+			if item == '#' {
+				count += 1
+			}
+		}
+	}
+	return count
+}
+
+func GetNewPixel(enhancement string, image []string, i int, j int, empty string) byte {
+	index := Get3x3IntVal(image, i, j, empty)
+	newPixel := enhancement[index]
+	// fmt.Printf(" newPixel: %v\n", newPixel)
+	return newPixel
+}
+
+func Get3x3IntVal(image []string, i int, j int, empty string) int {
+	pixelString := Get3x3String(image, i, j, empty)
+	binaryString := PixelStringToBinaryString(pixelString)
+	intval, err := strconv.ParseInt(binaryString, 2, 64)
+	// fmt.Printf("pixel: %v at (%v, %v)\n pixelSTring: %v\n binaryString: %v\n intval: %v\n", string(image[j][i]), i, j, pixelString, binaryString, intval)
+	if err != nil {
+		panic(err)
+	}
+	return int(intval)
+}
+
+func Get3x3String(image []string, i int, j int, empty string) string {
+	firstRow := ""
+	if j-1 >= 0 {
+		if i-1 >= 0 {
+			firstRow += string(image[j-1][i-1])
+		} else {
+			firstRow += empty
+		}
+		firstRow += string(image[j-1][i])
+		if i+1 < len(image[0]) {
+			firstRow += string(image[j-1][i+1])
+		} else {
+			firstRow += empty
+		}
+	} else {
+		firstRow = strings.Repeat(empty, 3)
+	}
+
+	secondRow := ""
+	if i-1 >= 0 {
+		secondRow += string(image[j][i-1])
+	} else {
+		secondRow += empty
+	}
+	secondRow += string(image[j][i])
+	if i+1 < len(image[0]) {
+		secondRow += string(image[j][i+1])
+	} else {
+		secondRow += empty
+	}
+
+	thirdRow := ""
+	if j+1 < len(image) {
+		if i-1 >= 0 {
+			thirdRow += string(image[j+1][i-1])
+		} else {
+			thirdRow += empty
+		}
+		thirdRow += string(image[j+1][i])
+		if i+1 < len(image[0]) {
+			thirdRow += string(image[j+1][i+1])
+		} else {
+			thirdRow += empty
+		}
+	} else {
+		thirdRow = strings.Repeat(empty, 3)
+	}
+	return firstRow + secondRow + thirdRow
+	// Assume we stay in bounds because of padding
+	// return string(image[j-1][i-1]) + string(image[j-1][i]) + string(image[j-1][i+1]) +
+	// string(image[j][i-1]) + string(image[j][i]) + string(image[j][i+1]) +
+	// string(image[j+1][i-1]) + string(image[j+1][i]) + string(image[j+1][i+1])
+}
+
+func PixelStringToBinaryString(pixels string) string {
+	result := ""
+	for _, c := range pixels {
+		if c == '.' {
+			result += "0"
+		} else if c == '#' {
+			result += "1"
+		} else {
+			log.Panicf("unknown pixel: %v\n", c)
+		}
+	}
+	return result
+}
+
+func PadImage(image []string, times int, char string) []string {
+	w := len(image[0])
+
+	// Make top rows
+	var topRows []string
+	for t := 0; t < times; t++ {
+		topRows = append(topRows, strings.Repeat(char, w))
+	}
+	// Make bottom rows
+	bottomRows := make([]string, len(topRows))
+	copy(bottomRows, topRows)
+
+	// Join rows
+	image2 := append(append(topRows, image...), bottomRows...)
+
+	// Add columns
+	var result []string
+	colPad := strings.Repeat(char, times)
+	for _, row := range image2 {
+		result = append(result, colPad+row+colPad)
+	}
+
+	return result
+}
+
+func ReadInput(path string) (string, []string) {
 	file, err := os.Open(path)
 	if err != nil {
 		panic(err)
@@ -21,5 +147,12 @@ func ReadInput(path string) []string {
 		panic(scanner.Err())
 	}
 
-	return lines
+	if len(lines) < 1 {
+		log.Panicf("could not read input\n")
+	}
+	enhancement := lines[0]
+
+	image := lines[2:]
+
+	return enhancement, image
 }
