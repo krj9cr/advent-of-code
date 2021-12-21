dcf7e051ab50725038f59e5644e7a9f21089c734
krj9cr_uva <krj9cr@virginia.edu>
2021-12-21 09:12:31 -0500

    Day 21 pt 1

--- year_2021/day_21/part1/part1.go
+++ year_2021/day_21/part1/part1.go
@@ -12,10 +12,78 @@ func main() {
 		fmt.Println("Missing param, provide input file path")
 		return
 	}
-	lines := day21.ReadInput(os.Args[1])
-	fmt.Printf("Input: %v\n", lines)
+	p1, p2 := day21.ReadInput(os.Args[1])
+	fmt.Printf("Player1 start: %v, Player2 start: %v\n", p1, p2)
 
-	// DO STUFF
+	boardSize := 10
 
-	// fmt.Printf("Result: %v\n", result)
+	diceMax := 100
+	diceCurr := 0
+	dieRolls := 0
+
+	winningScore := 1000
+	p1Score := 0
+	p2Score := 0
+
+	// Play forever
+	for {
+		// Until a player is past a score (checked below)
+
+		// Player 1 goes
+		rollSum := 0
+		var p1Rolls []int
+		// Roll the dice 3 times
+		for roll := 0; roll < 3; roll++ {
+			diceCurr += 1
+			p1Rolls = append(p1Rolls, diceCurr)
+			rollSum += diceCurr
+
+			dieRolls += 1
+			if diceCurr >= diceMax {
+				diceCurr = 0
+			}
+		}
+		p1 += rollSum
+		p1 %= boardSize
+		if p1 == 0 {
+			p1 = boardSize
+		}
+		p1Score += p1
+		if p1Score >= winningScore {
+			fmt.Printf("Player 1 wins with: %v\n", p1Score)
+			fmt.Printf("Player 2 has: %v points, die was rolled: %v\n", p2Score, dieRolls)
+			fmt.Printf("Result: %v\n", p2Score*dieRolls)
+			break
+		}
+
+		// Player 2 goes
+		rollSum = 0
+		var p2Rolls []int
+		// Roll the dice 3 times
+		for roll := 0; roll < 3; roll++ {
+			diceCurr += 1
+			p2Rolls = append(p2Rolls, diceCurr)
+			rollSum += diceCurr
+
+			dieRolls += 1
+			if diceCurr >= diceMax {
+				diceCurr = 0
+			}
+		}
+		p2 += rollSum
+		p2 %= boardSize
+		if p2 == 0 {
+			p2 = boardSize
+		}
+		p2Score += p2
+		if p2Score >= winningScore {
+			fmt.Printf("Player 2 wins with: %v\n", p2Score)
+			fmt.Printf("Player 1 has: %v points, die was rolled: %v\n", p1Score, dieRolls)
+			fmt.Printf("Result: %v\n", p1Score*dieRolls)
+			break
+		}
+
+		fmt.Printf("Player 1 rolls %v and moves to %v for a total score of %v\n", p1Rolls, p1, p1Score)
+		fmt.Printf("Player 2 rolls %v and moves to %v for a total score of %v\n", p2Rolls, p2, p2Score)
+	}
 }
--- year_2021/day_21/utils.go
+++ year_2021/day_21/utils.go
@@ -1,11 +1,14 @@
-package day0X
+package day21
 
 import (
 	"bufio"
+	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func ReadInput(path string) []string {
+func ReadInput(path string) (int, int) {
 	file, err := os.Open(path)
 	if err != nil {
 		panic(err)
@@ -21,5 +24,21 @@ func ReadInput(path string) []string {
 		panic(scanner.Err())
 	}
 
-	return lines
+	if len(lines) != 2 {
+		log.Panicf("could not read input\n")
+	}
+
+	p1 := strings.Split(lines[0], " ")[4]
+	p2 := strings.Split(lines[1], " ")[4]
+
+	i1, err := strconv.Atoi(p1)
+	if err != nil {
+		panic(err)
+	}
+	i2, err := strconv.Atoi(p2)
+	if err != nil {
+		panic(err)
+	}
+
+	return i1, i2
 }
