df6e208f7b9576b2f654c94489de197507f7e6ac
krj9cr_uva <krj9cr@virginia.edu>
2021-12-25 22:36:05 -0500

    Working on 23

--- year_2021/day_23/part1/part1.go
+++ year_2021/day_23/part1/part1.go
@@ -6,16 +6,18 @@ import (
 	day23 "year_2021/day_23"
 )
 
+// Was able to solve manually, but need to write something for part 2
 func main() {
 
 	if len(os.Args) < 2 {
 		fmt.Println("Missing param, provide input file path")
 		return
 	}
-	lines := day23.ReadInput(os.Args[1])
-	fmt.Printf("Input: %v\n", lines)
+	state := day23.ReadInput(os.Args[1])
+	state.PrintRooms()
 
-	// DO STUFF
+	// loop until we have a winning state
+	fmt.Printf("Done?: %v\n", day23.Done(rooms))
 
 	// fmt.Printf("Result: %v\n", result)
 }
