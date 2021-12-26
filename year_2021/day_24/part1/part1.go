4b1019c50a17730377dca50e7957f0991ab33fd1
krj9cr_uva <krj9cr@virginia.edu>
2021-12-25 19:10:51 -0500

    Day 24

--- year_2021/day_24/part1/part1.go
+++ year_2021/day_24/part1/part1.go
@@ -6,6 +6,15 @@ import (
 	day24 "year_2021/day_24"
 )
 
+/**
+input[4] == input[3] - 1
+input[5] == input[2] - 5
+input[8] == input[7] + 3
+input[9] == input[6] + 7
+input[11] == input[10] + 2
+input[12] == input[1] - 2
+input[13] == input[0] + 4
+**/
 func main() {
 
 	if len(os.Args) < 2 {
@@ -15,23 +24,32 @@ func main() {
 	lines := day24.ReadInput(os.Args[1])
 	// fmt.Printf("Input: %v\n", lines)
 
-	input := 99999999999999
+	input := 59998426997979
+	// tried := make(map[int]bool)
 	maxi := 0
+	steps := 0
 	for {
 		// input := day24.RandomInput()
-		// fmt.Printf("trying input: %v\n", input)
+		inputNum := input //day24.InputToNum(input)
+		// if _, ok := tried[inputNum]; ok {
+		// 	continue
+		// }
+		// tried[inputNum] = true
+		fmt.Printf("trying: %v\n", input)
 		variables := day24.ProcessLines(lines, day24.NumToInput(input))
-		// fmt.Printf("end: %v\n", variables)
+		fmt.Printf("end: %v\n", variables)
 		if variables["z"] == 0 {
-			// modelNumber := day24.InputToNum(input)
 			// fmt.Printf("Valid input: %v\n", modelNumber)
-			if input > maxi {
-				maxi = input
+			if inputNum > maxi {
+				maxi = inputNum
 				fmt.Printf("max: %v\n", maxi)
+				break // for now, stop
 			}
 		}
+		// if steps > 1 {
+		// 	break
+		// }
 		input -= 1
+		steps += 1
 	}
-
-	// fmt.Printf("Result: %v\n", result)
 }
--- year_2021/day_24/part2/part2.go
+++ year_2021/day_24/part2/part2.go
@@ -6,6 +6,15 @@ import (
 	day24 "year_2021/day_24"
 )
 
+/**
+input[4] == input[3] - 1
+input[5] == input[2] - 5
+input[8] == input[7] + 3
+input[9] == input[6] + 7
+input[11] == input[10] + 2
+input[12] == input[1] - 2
+input[13] == input[0] + 4
+**/
 func main() {
 
 	if len(os.Args) < 2 {
@@ -13,16 +22,34 @@ func main() {
 		return
 	}
 	lines := day24.ReadInput(os.Args[1])
-	fmt.Printf("Input: %v\n", lines)
+	// fmt.Printf("Input: %v\n", lines)
 
-	input := 10
-	variables := map[string]int{"w": 0, "x": 0, "y": 0, "z": 0}
-
-	for _, line := range lines {
-		day24.ProcessLine(line, variables, &input)
+	input := 13621111481315
+	// tried := make(map[int]bool)
+	maxi := 0
+	steps := 0
+	for {
+		// input := day24.RandomInput()
+		inputNum := input //day24.InputToNum(input)
+		// if _, ok := tried[inputNum]; ok {
+		// 	continue
+		// }
+		// tried[inputNum] = true
+		fmt.Printf("trying: %v\n", input)
+		variables := day24.ProcessLines(lines, day24.NumToInput(input))
+		fmt.Printf("end: %v\n", variables)
+		if variables["z"] == 0 {
+			// fmt.Printf("Valid input: %v\n", modelNumber)
+			if inputNum > maxi {
+				maxi = inputNum
+				fmt.Printf("max: %v\n", maxi)
+				break // for now, stop
+			}
+		}
+		// if steps > 1 {
+		// 	break
+		// }
+		input += 1
+		steps += 1
 	}
-
-	fmt.Printf("end: %v\n", variables)
-
-	// fmt.Printf("Result: %v\n", result)
 }
