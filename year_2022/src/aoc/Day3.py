b058fe5dabd438c8c60ff96ef69701b77c40a3d4
krj9cr_uva <krj9cr@virginia.edu>
2022-12-03 00:14:55 -0500

    Day 3

--- year_2022/src/aoc/Day3.py
+++ year_2022/src/aoc/Day3.py
@@ -8,14 +8,72 @@ def parseInput(day):
 def part1():
     lines = parseInput(3)
     print(lines)
+    letters = []
+    for line in lines:
+        half = len(line) // 2
+        # print("half: ", half)
+        first = line[:half]
+        second = line[half:]
+        for char in first:
+            if char in second:
+                letters.append(char)
+                break
+    score = 0
+    for letter in letters:
+        if letter.islower():
+            o = ord(letter) - 96
+            score += o
+            print(letter, o)
+        else:
+            o = ord(letter) - 64 + 27 - 1
+            score += o
+            print(letter, o)
+
+    print(score)
+
+def divide_chunks(l, n):
+    # looping till length l
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
 
 def part2():
     lines = parseInput(3)
-    print(lines)
+    groups = divide_chunks(lines, 3)
+    # print(lines)
+    letters = []
+    for group in groups:
+        print(group)
+        common = None
+        for line in group:
+            for char in line:
+                in_all = True
+                for line2 in group:
+                    if line != line2 and char not in line2:
+                        in_all = False
+                        break
+                if in_all:
+                    common = char
+                    break
+            if common:
+                break
+        letters.append(common)
+
+    score = 0
+    for letter in letters:
+        if letter.islower():
+            o = ord(letter) - 96
+            score += o
+            print(letter, o)
+        else:
+            o = ord(letter) - 64 + 27 - 1
+            score += o
+            print(letter, o)
+
+    print(score)
 
 if __name__ == "__main__":
-    print("\nPART 1 RESULT")
-    part1()
+    # print("\nPART 1 RESULT")
+    # part1()
 
-    # print("\nPART 2 RESULT")
-    # part2()
+    print("\nPART 2 RESULT")
+    part2()
