c8c6e785308c6b7ef158da03c9b014a4e08d1a7c
krj9cr_uva <krj9cr@virginia.edu>
2022-12-06 18:16:44 -0500

    Prep day 7

---
+++ year_2022/src/Day07.py
@@ -0,0 +1,30 @@
+import time
+
+def parseInput(day):
+    dayf = "{:02d}".format(day)
+    path = __file__.rstrip(f"Day{dayf}.py") + f"../input/day{dayf}.txt"
+    with open(path, 'r') as file:
+        lines = [line.strip() for line in file]
+        return lines
+
+def part1():
+    lines = parseInput(X)
+    print(lines)
+q
+
+def part2():
+    lines = parseInput(X)
+    print(lines)
+
+if __name__ == "__main__":
+    print("\nPART 1 RESULT")
+    start = time.perf_counter()
+    part1()
+    end = time.perf_counter()
+    print("Time:", end - start)
+
+    # print("\nPART 2 RESULT")
+    # start = time.perf_counter()
+    # part2()
+    # end = time.perf_counter()
+    # print("Time:", end - start)
