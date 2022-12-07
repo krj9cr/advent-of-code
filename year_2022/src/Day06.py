da0bbca24f58d31bbb507b0ad797511804d90752
krj9cr_uva <krj9cr@virginia.edu>
2022-12-06 20:59:40 -0500

    time differently, optimize day 6

--- year_2022/src/Day06.py
+++ year_2022/src/Day06.py
@@ -7,63 +7,45 @@ def parseInput(day):
         lines = [line.strip("") for line in file][0]
         return lines
 
-def part1():
-    line = parseInput(6)
-    # print(line)
-
+def check(line, windowSize):
     for i in iter(range(len(line) - 2)):
-        window = line[i:i + 4]
+        window = line[i:i + windowSize]
         # print(window)
         charcount = {}
+
+        allOnes = True
         for char in window:
             if charcount.get(char) is not None:
-                charcount[char] += 1
-            else:
-                charcount[char] = 1
-        allOnes = True
-        for key, val in charcount.items():
-            if val > 1:
                 allOnes = False
                 break
+            else:
+                charcount[char] = 1
         # print(charcount)
         if allOnes:
             print(window)
-            print(line.index(window)+4)
+            print(line.index(window)+windowSize)
             break
+
+
+def part1():
+    line = parseInput(6)
+    # print(line)
+    check(line, 4)
 
 def part2():
     line = parseInput(6)
     # print(line)
-
-    for i in iter(range(len(line) - 2)):
-        window = line[i:i + 14]
-        # print(window)
-        charcount = {}
-        for char in window:
-            if charcount.get(char) is not None:
-                charcount[char] += 1
-            else:
-                charcount[char] = 1
-        allOnes = True
-        for key, val in charcount.items():
-            if val > 1:
-                allOnes = False
-                break
-        # print(charcount)
-        if allOnes:
-            print(window)
-            print(line.index(window)+14)
-            break
+    check(line, 14)
 
 if __name__ == "__main__":
     print("\nPART 1 RESULT")
     start = time.perf_counter()
     part1()
     end = time.perf_counter()
-    print("Time:", end - start)
+    print("Time (ms):", (end - start) * 1000)
 
     print("\nPART 2 RESULT")
     start = time.perf_counter()
     part2()
     end = time.perf_counter()
-    print("Time:", end - start)
+    print("Time (ms):", (end - start) * 1000)
