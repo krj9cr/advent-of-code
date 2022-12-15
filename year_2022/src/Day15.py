b2e8bed99ab0508bc0161262e016369db288d2b5
krj9cr_uva <krj9cr@virginia.edu>
2022-12-15 09:23:24 -0500

    Start 15

--- year_2022/src/Day15.py
+++ year_2022/src/Day15.py
@@ -5,11 +5,56 @@ def parseInput(day):
     path = __file__.rstrip(f"Day{dayf}.py") + f"../input/day{dayf}.txt"
     with open(path, 'r') as file:
         lines = [line.strip() for line in file]
-        return lines
+        beacons = []
+        sensors = []
+        for line in lines:
+            split = line.split(":")
+            for item in split:
+                split2 = item.split("=")
+                if split2[0].startswith("Sensor"):
+                    sensors.append((int(split2[1].split(",")[0]), int(split2[2])))
+                else:
+                    beacons.append((int(split2[1].split(",")[0]), int(split2[2])))
+        return sensors, beacons
+
+def manhattan(one, two):
+    return abs(one[0] - two[0]) + abs(one[1] - two[1])
+
+def get_diamond_neighbors(center, y, n=1):
+    ret = []
+    for dx in range(-n, n + 1):
+        ydiff = n - abs(dx)
+        if center[1]+ -ydiff <= y <= center[1] +ydiff + 1:
+            ret.append((center[0] + dx, y))
+    return ret
 
 def part1():
-    lines = parseInput(15)
-    print(lines)
+    sensors, beacons = parseInput(15)
+    print(sensors)
+    num_sensors = len(sensors)
+    print(beacons)
+    shortest_dists = []
+    for i in range(num_sensors):
+        dist = manhattan(sensors[i], beacons[i])
+        shortest_dists.append(dist)
+    print(shortest_dists)
+
+    y = 2000000
+    y_no_beacons = set()
+
+    # generate some diamonds/ranges
+    for i in range(num_sensors):
+        print("Sensor ", i, "out of ", num_sensors)
+        sensor = sensors[i]
+        neighbors = get_diamond_neighbors(sensor, y, shortest_dists[i])
+        for n in neighbors:
+            if n[1] == y and n not in beacons and n not in sensors:
+                y_no_beacons.add(n)
+    print(sorted(y_no_beacons))
+    print(len(y_no_beacons))
+
+# 4502209 too high
+# 5543957 too high
 
 
 def part2():
