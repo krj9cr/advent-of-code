85152de51213fb3f47b6d4d02a502703b99093bd
krj9cr_uva <krj9cr@virginia.edu>
2022-12-18 20:16:39 -0500

    Day 15 pt 2

--- year_2022/src/Day15.py
+++ year_2022/src/Day15.py
@@ -20,26 +20,39 @@ def parseInput(day):
 def manhattan(one, two):
     return abs(one[0] - two[0]) + abs(one[1] - two[1])
 
+# https://www.geeksforgeeks.org/merging-intervals/
+def mergeIntervals(intervals):
+    # Sort the array on the basis of start values of intervals.
+    intervals.sort()
+    stack = [intervals[0]]
+    # insert first interval into stack
+    for i in intervals[1:]:
+        # Check for overlapping interval,
+        # if interval overlap
+        if stack[-1][0] <= i[0] <= stack[-1][-1]:
+            stack[-1][-1] = max(stack[-1][-1], i[-1])
+        else:
+            stack.append(i)
+
+    # print("The Merged Intervals are :", end=" ")
+    # for i in range(len(stack)):
+    #     print(stack[i], end=" ")
+    return stack
+
 def part1():
     sensors, beacons = parseInput(15)
     print(sensors)
     num_sensors = len(sensors)
     print(beacons)
-    shortest_dists = []
+    # shortest_dists = []
+    y = 2000000
+    # y = 10
+    y_no_beacons = set()
+    ranges = []
     for i in range(num_sensors):
         dist = manhattan(sensors[i], beacons[i])
-        shortest_dists.append(dist)
-    # print(shortest_dists)
-
-    y = 2000000
-    y_no_beacons = set()
-
-    # generate some diamonds/ranges
-    for i in range(num_sensors):
+        # shortest_dists.append(dist)
         sensor = sensors[i]
-        dist = shortest_dists[i]
-        print("Sensor ", i, "out of ", num_sensors)
-        # print(sensor, "dist", dist, "checking", sensor[0]+-dist, "to", sensor[0]+dist)
 
         # check "diamond", but only in row y
         # inspired by: https://stackoverflow.com/questions/64823023/determining-neighbours-of-cell-as-diamond-shape-in-python
@@ -49,17 +62,16 @@ def part1():
             # figure out which "row" we're in
             row = abs(y - sensor[1])
             # print("row", row)
-            xdiff = dist - abs(row)
-            # print("xdiff",xdiff)
-            for dx in range(-xdiff, xdiff + 1):
-                no_beacon = (sensor[0] + dx, y)
-                # print(no_beacon)
-                if no_beacon not in beacons:
-                    y_no_beacons.add(no_beacon[0])
+            xdiff = dist - row
+            x_min = sensor[0] - xdiff
+            x_max = sensor[0] + xdiff
+            ranges.append((x_min, x_max))
 
-        print()
-    # print(sorted(y_no_beacons))
-    print(len(y_no_beacons))
+    sorted_ranges = list(sorted(ranges))
+
+    merged = mergeIntervals([ list(item) for item in sorted_ranges])
+    print(merged)
+    print(merged[0][1]-merged[0][0])
 
 
 def part2():
@@ -67,41 +79,50 @@ def part2():
     print(sensors)
     num_sensors = len(sensors)
     print(beacons)
-    shortest_dists = []
+    # shortest_dists = []
+    y = 4000000
+    # y = 20
+    y_no_beacons = set()
+    ranges = {}
     for i in range(num_sensors):
         dist = manhattan(sensors[i], beacons[i])
-        shortest_dists.append(dist)
-    # print(shortest_dists)
-
-    max_x_y = 20
-    y_no_beacons = set()
-
-    # generate some diamonds/ranges
-    for i in range(num_sensors):
+        # shortest_dists.append(dist)
         sensor = sensors[i]
-        dist = shortest_dists[i]
-        print("Sensor ", i, "out of ", num_sensors)
 
-        # figure out if we're in range of x and y
-        # have a set of all possible coords? ...
-        # remove items from the set
+        # check "diamond", but only in row y
+        # inspired by: https://stackoverflow.com/questions/64823023/determining-neighbours-of-cell-as-diamond-shape-in-python
+        # figure out if we're in range of y and x
+        for j in range(max(0, sensor[1] - dist), min(y + 1, sensor[1] + dist)):
+            # print(j)
+            # if sensor[1] - dist <= j <= :
+                # print(sensor, "in range")
+                # figure out which "row" we're in
+            row = abs(sensor[1] - j)
 
-        # if sensor[1] - dist <= y <= sensor[1] + dist:
-            # print(sensor, "in range")
-            # figure out which "row" we're in
-            # row = abs(y - sensor[1])
             # print("row", row)
-            # xdiff = dist - abs(row)
-            # print("xdiff",xdiff)
-            # for dx in range(-xdiff, xdiff + 1):
-                #no_beacon = (sensor[0] + dx, y)
-                # print(no_beacon)
-                #if no_beacon not in beacons:
-                    #y_no_beacons.add(no_beacon[0])
+            xdiff = dist - row
+            x_min = sensor[0] - xdiff
+            x_max = sensor[0] + xdiff
+            if ranges.get(j) is None:
+                ranges[j] = [[x_min, x_max]]
+            else:
+                ranges[j] = ranges[j] + [[x_min, x_max]]
 
-        print()
-    # print(sorted(y_no_beacons))
-    print(len(y_no_beacons))
+    check_ranges = {}
+    for j in ranges:
+        y_ranges = ranges[j]
+        sorted_ranges = list(sorted(y_ranges))
+
+        merged = mergeIntervals([ list(item) for item in sorted_ranges])
+        if len(merged) > 1:
+            check_ranges[j] = merged
+            if len(merged) == 2 and merged[1][0] -1 == merged[0][1]:
+                continue
+            else:
+                print(j, merged)
+        # print(merged[0][1]-merged[0][0])
+
+    # TODO: manually did math at the end
 
 if __name__ == "__main__":
     # print("\nPART 1 RESULT")
