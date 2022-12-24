36620087f26f926572169dcbfdb71d82e037b534
krj9cr_uva <krj9cr@virginia.edu>
2022-12-24 10:36:48 -0500

    Prep day 24

--- year_2022/src/Day20.py
+++ year_2022/src/Day20.py
@@ -9,6 +9,11 @@ def parseInput(day):
         lines = [int(line.strip()) for line in file]
         return lines
 
+# https://stackoverflow.com/questions/3883004/the-modulo-operation-on-negative-numbers-in-python
+def newMod(a,b):
+    res = a%b
+    return res if not res else res-b if a<0 else res
+
 def part1():
     lines = parseInput(20)
     print(lines)
@@ -20,11 +25,7 @@ def part1():
         if num != 0:
             index = lines.index(num)
             new_index = index + num
-            if new_index < 0:
-                while new_index < 0:
-                    new_index = size + new_index - 1
-            else:
-                new_index = new_index % size
+            new_index = newMod(new_index, size)
             del lines[index]
             # print(lines)
             # print("moving num:", num, "to", new_index)
@@ -46,6 +47,10 @@ def part1():
     print("answer:", first + second + third)
 
 
+# 20373 too high
+# 5270 too high
+
+
 
 def part2():
     lines0 = parseInput(20)
@@ -63,6 +68,7 @@ def part2():
         if num != 0:
             index = lines.index(num)
             new_index = index + num
+            math.rem
             if new_index < 0:
                 while new_index < 0: # TODO: need euclidean remainder or else it's too slow
                     new_index = size + new_index - 1
@@ -87,6 +93,7 @@ def part2():
     third = lines[third_index]
     print("result:", first, second, third)
     print("answer:", first + second + third)
+
 
 if __name__ == "__main__":
     print("\nPART 1 RESULT")
