64845fcc94ba1723e37df500f912932615c7912f
krj9cr_uva <krj9cr@virginia.edu>
2022-12-24 13:41:39 -0500

    Day 24 pt 2

--- year_2022/src/Day24.py
+++ year_2022/src/Day24.py
@@ -1,5 +1,4 @@
 import heapq
-import sys
 import time
 
 class Blizzard:
@@ -59,6 +58,7 @@ def parseInput(day):
         end = (w-2, h-1)
         return w, h, blizzards, walls, start, end
 
+# just used this to make sure all our coors were right initially
 def print_grid(w, h, blizzards, walls):
     for j in range(h):
         for i in range(w):
@@ -76,7 +76,6 @@ def print_grid(w, h, blizzards, walls):
         print()
     print()
 
-
 # A custom priority queue used for A Star Search below
 class PriorityQueue:
     def __init__(self):
@@ -86,6 +85,7 @@ class PriorityQueue:
         return len(self.elements) == 0
 
     def put(self, item, priority):
+        # we avoid having duplicates, since we don't need to reprocess them
         if (priority, item) not in self.elements:
             heapq.heappush(self.elements, (priority, item))
 
@@ -102,74 +102,10 @@ def heuristic(goal, x2, y2):
     dist = abs(x1 - x2) + abs(y1 - y2)
     return dist
 
-# See: https://www.redblobgames.com/pathfinding/a-star/implementation.html
-# board  - a 2d array of strings or numbers
-# start  - a tuple of numbers representing the starting coordinate in the 2d grid
-# goal   - a tuple of numbers representing the ending coordinate in the 2d grid
-def a_star_search(w, h, walls, blizzards, start, goal):
-    # init
-    queue = PriorityQueue()
-    came_from = {}  # keeps track of our path to the goal
-    cost_so_far = {}  # keeps track of cost to arrive at ((x, y))
-
-    # add start infos
-    queue.put((start[0], start[1], 0), 0)
-    came_from[(start[0], start[1], 0)] = None # coord and minutes waited there
-    cost_so_far[(start[0], start[1], 0)] = 0
-
-    while not queue.empty():
-        x, y, minutes_waited = queue.get()
-
-        if (x, y) == goal:
-            break
-
-        # move each blizzard
-        blizzard_coords = []
-        for b in blizzards:
-            b.move(w, h)
-            blizzard_coords.append(b.coord)
-
-        # move our spot
-        can_move = False
-        cost_to_move = 0 # this increments the minute we're on
-        new_cost = cost_so_far[(x, y, minutes_waited)] + cost_to_move
-        # for each adjacent square
-        for x2, y2 in ((x, y + 1), (x + 1, y), (x - 1, y), (x, y - 1)):
-            next_spot = (x2, y2)
-            # if we're in bounds
-            if 0 <= x2 < w and 0 <= y2 < h:
-                # if we're not hitting a wall or blizzard
-                if next_spot not in walls and next_spot not in blizzard_coords:
-                    if next_spot not in cost_so_far or new_cost < cost_so_far[(x2, y2, minutes_waited)]:
-                        cost_so_far[(x2, y2, minutes_waited)] = new_cost
-                        priority = new_cost + heuristic(goal, x2, y2)
-                        queue.put((x2, y2, minutes_waited), priority)
-                        came_from[(x2, y2, minutes_waited)] = (x, y)
-        # TODO: add the option of waiting
-        if (x, y) not in blizzard_coords:
-            print("adding option to wait at ", x,y)
-            cost_so_far[(x, y, minutes_waited+1)] = new_cost + 1
-            priority = new_cost + 1 + heuristic(goal, x, y)
-            queue.put((x, y, minutes_waited+1), priority)
-
-        print(queue)
-    return came_from, cost_so_far
-
-def part1():
-    w, h, blizzards, walls, start, end = parseInput(24)
-    print_grid(w, h, blizzards, walls)
-
-    # print(blizzards)
-    # print(walls)
-    print("start", start, "end", end)
-
-    cap = (w + h) * 2
-
-    minute = 0
-    spots = PriorityQueue() # use a queue to prioritize the path we take
-    spots.put(start, 0) # keep track of (x, y), as if we were simultaneously in each spot
+def travel(start, end, w, h, walls, blizzards, minute=0):
+    spots = PriorityQueue()  # use a queue to prioritize the path we take
+    spots.put(start, 0)  # keep track of (x, y), as if we were simultaneously in each spot
     while True:
-        # print(spots)
         # move all the blizzards
         blizzard_coords = []
         for b in blizzards:
@@ -192,22 +128,36 @@ def part1():
                         next_spots.put(next_spot, cost)
                         # if any spot is the end goal, return minutes_elapsed
                         if next_spot == end:
-                            print("made it to the end! in ", minute+1, "Minutes")
-                            return
+                            print("made it to the end! in ", minute + 1, "Minutes")
+                            return minute + 1, blizzards
             # add waiting at the current spot as an option, if it's open
             if (x, y) not in blizzard_coords:
                 cost = minute + heuristic(end, x, y)
                 next_spots.put((x, y), cost)
         spots = next_spots
-        print(spots)
+        # print(spots.elements[0])
 
         minute += 1
+
+def part1():
+    w, h, blizzards, walls, start, end = parseInput(24)
+    # print("start", start, "end", end)
+
+    travel(start, end, w, h, walls, blizzards, minute=0)
+
 
 
 
 def part2():
-    lines = parseInput(24)
-    print(lines)
+    w, h, blizzards, walls, start, end = parseInput(24)
+    # print("start", start, "end", end)
+
+    start_to_end, blizzards = travel(start, end, w, h, walls, blizzards, minute=0)
+    start2, end2 = (end, start)
+    end_to_start, blizzards = travel(start2, end2, w, h, walls, blizzards, minute=start_to_end)
+    start3, end3 = (start, end)
+    _, _ = travel(start3, end3, w, h, walls, blizzards, minute=end_to_start)
+
 
 if __name__ == "__main__":
     print("\nPART 1 RESULT")
@@ -216,8 +166,8 @@ if __name__ == "__main__":
     end_time = time.perf_counter()
     print("Time (ms):", (end_time - start_time) * 1000)
 
-    # print("\nPART 2 RESULT")
-    # start_time = time.perf_counter()
-    # part2()
-    # end_time = time.perf_counter()
-    # print("Time (ms):", (end_time - start_time) * 1000)
+    print("\nPART 2 RESULT")
+    start_time = time.perf_counter()
+    part2()
+    end_time = time.perf_counter()
+    print("Time (ms):", (end_time - start_time) * 1000)
