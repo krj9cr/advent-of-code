688c38b79074c6a2a5c9dd60e6f49e1ab7920223
krj9cr_uva <krj9cr@virginia.edu>
2022-12-24 21:22:50 -0500

    Day 20 pt 2

--- year_2022/src/Day20.py
+++ year_2022/src/Day20.py
@@ -32,24 +32,15 @@ def get_new_index(item, sequence, size):
     new_index = new_index % (size - 1)
     return index, new_index
 
-def part1():
-    items, size, zero_item = parseInput(20)
-    # print(items)
-
-    original_items = copy.copy(items)
-
+def mix(original_items, items, size):
     for item in original_items:
         if item.value == 0: # ignore 0 since it doesn't move
             continue
         index, new_index = get_new_index(item, items, size)
         old_item = items.pop(index)
-        # print(lines)
-        # print("moving num:", item.value, "to", new_index)
         items.insert(new_index, old_item)
-        # print(items)
-        # print()
 
-    # find zero
+def find_answer(items, size, zero_item):
     zero_index = items.index(zero_item)
     first_index = (zero_index + 1000) % size
     second_index = (zero_index + 2000) % size
@@ -63,63 +54,39 @@ def part1():
     print("answer:", first + second + third)
 
 
-# 20373 too high
-# 5270 too high
-# 5195 wrong
-
+def part1():
+    items, size, zero_item = parseInput(20)
+    original_items = copy.copy(items)
+    mix(original_items, items, size)
+    find_answer(items, size, zero_item)
 
 def part2():
-    lines0 = parseInput(20)
-    # print(lines)
-    size = len(lines0)
+    items, size, zero_item = parseInput(20)
 
     multiplier = 811589153
-    lines = []
-    for num in lines0:
-        lines.append(num * multiplier)
+    for item in items:
+        item.value = item.value * multiplier
+    # print(items)
+    original_items = copy.copy(items)
 
-    original_lines = copy.deepcopy(lines)
+    for i in range(10):
+        mix(original_items, items, size)
 
-    for num in original_lines:
-        if num != 0:
-            index = lines.index(num)
-            new_index = index + num
-            math.rem
-            if new_index < 0:
-                while new_index < 0: # TODO: need euclidean remainder or else it's too slow
-                    new_index = size + new_index - 1
-            else:
-                new_index = new_index % size
-            del lines[index]
-            # print(lines)
-            # print("moving num:", num, "to", new_index)
-            lines.insert(new_index, num)
-        # print(lines)
-        # print()
+    find_answer(items, size, zero_item)
 
-    # find zero
-    zero_index = lines.index(0)
-    first_index = (zero_index + 1000) % size
-    second_index = (zero_index + 2000) % size
-    third_index = (zero_index + 3000) % size
 
-    # get
-    first = lines[first_index]
-    second = lines[second_index]
-    third = lines[third_index]
-    print("result:", first, second, third)
-    print("answer:", first + second + third)
+
 
 
 if __name__ == "__main__":
-    print("\nPART 1 RESULT")
-    start = time.perf_counter()
-    part1()
-    end = time.perf_counter()
-    print("Time (ms):", (end - start) * 1000)
-
-    # print("\nPART 2 RESULT")
+    # print("\nPART 1 RESULT")
     # start = time.perf_counter()
-    # part2()
+    # part1()
     # end = time.perf_counter()
     # print("Time (ms):", (end - start) * 1000)
+
+    print("\nPART 2 RESULT")
+    start = time.perf_counter()
+    part2()
+    end = time.perf_counter()
+    print("Time (ms):", (end - start) * 1000)
