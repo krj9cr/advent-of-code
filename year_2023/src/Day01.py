d190574dddfda70dab75fe94bdf3fda02fa8fb8b
krj9cr_uva <krj9cr@virginia.edu>
2023-12-01 17:50:58 -0500

    Cleanup and make Day 1 slightly less awful

--- year_2023/src/Day01.py
+++ year_2023/src/Day01.py
@@ -4,16 +4,6 @@ def parseInput(day):
     dayf = "{:02d}".format(day)
     path = __file__.rstrip(f"Day{dayf}.py") + f"../input/day{dayf}.txt"
     with open(path, 'r') as file:
-        # lines = [line.strip() for line in file]
-        # groups = []
-        # currGroup = 0
-        # for line in lines:
-        #     if line == "":
-        #         groups.append(currGroup)
-        #         currGroup = 0
-        #     else:
-        #         currGroup += int(line)
-        # return groups
         lines = [line.strip() for line in file]
         return lines
 
@@ -23,15 +13,11 @@ def part1():
     for line in lines:
         numStr = ""
         for char in line:
-            try:
-                c = int(char)
+            if char.isdecimal():
                 numStr += char
-            except:
-                continue
         num = int(numStr[0] + numStr[-1])
         nums.append(num)
     print(sum(nums))
-    # print(max(lines))
 
 def part2():
     lines = parseInput(1)
@@ -40,54 +26,57 @@ def part2():
     for line in lines:
         numStr = ""
         i = 0
+        buf3 = ""
+        buf4 = ""
+        buf5 = ""
         while i < len(line):
             char = line[i]
-            # print(char)
-            if char == "o":
-                if i+2 < len(line) and line[i+1] == "n" and line[i+2] == "e":
+            if char.isalpha():
+                buf3 += char
+                buf4 += char
+                buf5 += char
+                if len(buf3) > 3:
+                    buf3 = buf3[1:4]
+                if len(buf4) > 4:
+                    buf4 = buf4[1:5]
+                if len(buf5) > 5:
+                    buf5 = buf5[1:6]
+                # print(buf3, buf4, buf5)
+                if buf3 == "one":
                     numStr += "1"
-            elif char == "t":
-                if i+2 < len(line) and line[i+1] == "w" and line[i+2] == "o":
+                if buf3 == "two":
                     numStr += "2"
-                elif i+4 < len(line) and line[i+1] == "h" and line[i+2] == "r" and line[i+3] == "e" and line[i+4] == "e":
-                    numStr += "3"
-            elif char == "f":
-                if i+3 < len(line) and line[i+1] == "o" and line[i+2] == "u" and line[i+3] == "r":
-                    numStr += "4"
-                elif i+3 < len(line) and line[i+1] == "i" and line[i+2] == "v" and line[i+3] == "e":
-                    numStr += "5"
-            elif char == "s":
-                if i+2 < len(line) and line[i+1] == "i" and line[i+2] == "x":
+                if buf3 == "six":
                     numStr += "6"
-                elif i+4 < len(line) and line[i+1] == "e" and line[i+2] == "v" and line[i+3] == "e" and line[i+4] == "n":
-                    numStr += "7"
-            elif char == "e":
-                if i+4 < len(line) and line[i+1] == "i" and line[i+2] == "g" and line[i+3] == "h" and line[i+4] == "t":
-                    numStr += "8"
-            elif char == "n":
-                if i+3 < len(line) and line[i+1] == "i" and line[i+2] == "n" and line[i+3] == "e":
+                if buf4 == "four":
+                    numStr += "4"
+                if buf4 == "five":
+                    numStr += "5"
+                if buf4 == "nine":
                     numStr += "9"
+                if buf5 == "three":
+                    numStr += "3"
+                if buf5 == "seven":
+                    numStr += "7"
+                if buf5 == "eight":
+                    numStr += "8"
             else:
-                try:
-                    c = int(char)
-                    numStr += char
-                except:
-                    True
+                numStr += char
 
             i += 1
         # print(numStr)
         num = int(numStr[0] + numStr[-1])
         nums.append(num)
-        print(num)
+        # print(num)
     print(sum(nums))
 
 if __name__ == "__main__":
-    # print("\nPART 1 RESULT")
-    # start = time.perf_counter()
-    # part1()
-    # end = time.perf_counter()
-    # total = end - start
-    # print("Time (ms):", (end - start) * 1000)
+    print("\nPART 1 RESULT")
+    start = time.perf_counter()
+    part1()
+    end = time.perf_counter()
+    total = end - start
+    print("Time (ms):", (end - start) * 1000)
 
     print("\nPART 2 RESULT")
     start = time.perf_counter()
