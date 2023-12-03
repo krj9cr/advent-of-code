0cf5c51439097e4d2aef42c2d27faf86e1b2a465
krj9cr_uva <krj9cr@virginia.edu>
2023-12-03 09:31:30 -0500

    Day 3

--- year_2023/src/Day03.py
+++ year_2023/src/Day03.py
@@ -1,26 +1,65 @@
 import time
 
+class PartNumber:
+    def __init__(self, id, value, coords):
+        self.id = id
+        self.value = value
+        self.coords = set(coords)
+
+    def __hash__(self):
+        return hash((self.id, self.value, tuple(self.coords)))
+
+    def __str__(self):
+        return str(self.id) + ": " + str(self.value) + ": " + str(self.coords)
+
+class PartSymbol:
+    def __init__(self, value, coord):
+        self.value = value
+        self.coord = coord
+
+    def __str__(self):
+        return str(self.value) + ": " + str(self.coord)
+
 def parseInput(day):
     dayf = "{:02d}".format(day)
     path = __file__.rstrip(f"Day{dayf}.py") + f"../input/day{dayf}.txt"
     with open(path, 'r') as file:
         lines = [line.strip() for line in file]
+
+        # raw data as 2d array
         rows = []
-        # num_coords = {}
         for line in lines:
             row = []
-            # curr_digit = ""
             for char in line:
-                # if char.isdecimal():
-                #     curr_digit += char
-                # else:
-                #     if curr_digit != "":
-                #         row.append(curr_digit)
-                #     curr_digit = ""
-                #     row.append(char)
                 row.append(char)
             rows.append(row)
-        return rows
+
+        # find numbers and symbols
+        part_numbers = []
+        part_id = 0
+        part_symbols = []
+        for y in range(0, len(lines)):
+            row = lines[y]
+            curr_digit = ""
+            curr_coords = []
+            for x in range(len(row)):
+                char = row[x]
+                if char.isdecimal():
+                    curr_digit += char
+                    curr_coords.append(tuple([x, y]))
+                else:
+                    if curr_digit != "":
+                        part_numbers.append(PartNumber(part_id, int(curr_digit), curr_coords))
+                        part_id += 1
+                    curr_digit = ""
+                    curr_coords = []
+                    # it's a symbol
+                    if char != ".":
+                        part_symbols.append(PartSymbol(char, tuple([x, y])))
+            if curr_digit != "":
+                part_numbers.append(PartNumber(part_id, int(curr_digit), curr_coords))
+
+        return rows, part_numbers, part_symbols
 
 def get_neighbors(x, y, w, h):
     neighbors = []
@@ -31,60 +70,55 @@ def get_neighbors(x, y, w, h):
     return neighbors
 
 def part1():
-    lines = parseInput(3)
+    rows, part_numbers, part_symbols = parseInput(3)
 
-    h = len(lines)
-    w = len(lines[0])
+    h = len(rows)
+    w = len(rows[0])
 
-    # find numbers
-    num_coords = {}
-    for y in range(0, len(lines)):
-        row = lines[y]
-        curr_digit = ""
-        curr_coords = []
-        for x in range(len(row)):
-            char = row[x]
-            if char.isdecimal():
-                curr_digit += char
-                curr_coords.append([x, y])
-            else:
-                if curr_digit != "":
-                    for coord in curr_coords:
-                        num_coords[tuple(coord)] = int(curr_digit)
-                    # num_coords[curr_digit] = curr_coords
-                curr_digit = ""
-                curr_coords = []
-        if curr_digit != "":
-            for coord in curr_coords:
-                num_coords[tuple(coord)] = int(curr_digit)
-    print(num_coords)
-
-    # find symbols and get their neighbors
     nums_to_add = set()
-    for y in range(0, len(lines)):
-        row = lines[y]
-        for x in range(len(row)):
-            char = row[x]
-            if not char.isdecimal() and char != ".":
-                print(x, y, ":", char)
-                for nx, ny in get_neighbors(x, y, w, h):
-                    neighbor = lines[ny][nx]
-                    print("checking neighbor:",nx,ny, neighbor)
-                    if neighbor.isdecimal():
-                        # if num_coords.get(tuple([nx, ny])):
-                        nums_to_add.add(num_coords[tuple([nx, ny])])
-    print(nums_to_add)
-    print(sum(nums_to_add))
-
-    # not 334580
-    # not 335331
-    # not 336560
-
+    # for each symbol, check its neighbors for part_numbers
+    for part_symbol in part_symbols:
+        print(part_symbol)
+        for nx, ny in get_neighbors(part_symbol.coord[0], part_symbol.coord[1], w, h):
+            if rows[ny][nx].isdecimal():
+                # lookup the coords in the part_numbers
+                for part_number in part_numbers:
+                    if (nx, ny) in part_number.coords:
+                        print("adding: ", part_number)
+                        nums_to_add.add(part_number)
+    answer = 0
+    for n in nums_to_add:
+        print(n)
+        answer += n.value
+    print(answer)
 
 
 def part2():
-    lines = parseInput(3)
-    print(lines)
+    rows, part_numbers, part_symbols = parseInput(3)
+
+    h = len(rows)
+    w = len(rows[0])
+
+    gear_ratios = []
+    # for each symbol, check its neighbors for part_numbers
+    for part_symbol in part_symbols:
+        print(part_symbol)
+        nums_to_add = set()
+        for nx, ny in get_neighbors(part_symbol.coord[0], part_symbol.coord[1], w, h):
+            if rows[ny][nx].isdecimal():
+                # lookup the coords in the part_numbers
+                for part_number in part_numbers:
+                    if (nx, ny) in part_number.coords:
+                        print("adding: ", part_number)
+                        nums_to_add.add(part_number)
+        # check if it's a 'gear'
+        gear_ratio = 1
+        if len(nums_to_add) == 2:
+            for n in nums_to_add:
+                gear_ratio = gear_ratio * n.value
+            print(gear_ratio)
+            gear_ratios.append(gear_ratio)
+    print(sum(gear_ratios))
 
 if __name__ == "__main__":
     print("\nPART 1 RESULT")
@@ -93,8 +127,8 @@ if __name__ == "__main__":
     end = time.perf_counter()
     print("Time (ms):", (end - start) * 1000)
 
-    # print("\nPART 2 RESULT")
-    # start = time.perf_counter()
-    # part2()
-    # end = time.perf_counter()
-    # print("Time (ms):", (end - start) * 1000)
+    print("\nPART 2 RESULT")
+    start = time.perf_counter()
+    part2()
+    end = time.perf_counter()
+    print("Time (ms):", (end - start) * 1000)
