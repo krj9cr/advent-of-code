a94e4b3c5a5220c4ad1c23905af47b28716c9ada
krj9cr_uva <krj9cr@virginia.edu>
2023-12-03 09:40:37 -0500

    Day 3 cleanup and optimizations

--- year_2023/src/Day03.py
+++ year_2023/src/Day03.py
@@ -26,24 +26,24 @@ def parseInput(day):
     with open(path, 'r') as file:
         lines = [line.strip() for line in file]
 
+        h = len(lines)
+        w = len(lines[0])
+
         # raw data as 2d array
         rows = []
-        for line in lines:
-            row = []
-            for char in line:
-                row.append(char)
-            rows.append(row)
 
         # find numbers and symbols
         part_numbers = []
         part_id = 0
         part_symbols = []
-        for y in range(0, len(lines)):
+        for y in range(0, h):
+            raw_row = []
             row = lines[y]
             curr_digit = ""
             curr_coords = []
-            for x in range(len(row)):
+            for x in range(0, w):
                 char = row[x]
+                raw_row.append(char)
                 if char.isdecimal():
                     curr_digit += char
                     curr_coords.append(tuple([x, y]))
@@ -56,69 +56,64 @@ def parseInput(day):
                     # it's a symbol
                     if char != ".":
                         part_symbols.append(PartSymbol(char, tuple([x, y])))
+            rows.append(raw_row)
             if curr_digit != "":
                 part_numbers.append(PartNumber(part_id, int(curr_digit), curr_coords))
 
-        return rows, part_numbers, part_symbols
+        # convert part_numbers to a dict for easy coordinate lookups
+        part_numbers_dict = {}
+        for part_number in part_numbers:
+            for coord in part_number.coords:
+                part_numbers_dict[coord] = part_number
+
+        return rows, w, h, part_numbers, part_symbols, part_numbers_dict
 
 def get_neighbors(x, y, w, h):
     neighbors = []
-    for nx, ny in (x - 1, y - 1), (x, y - 1), (x + 1, y - 1), (x - 1, y), (x + 1, y), (x - 1, y + 1), (x, y + 1), (
-    x + 1, y + 1):
+    for nx, ny in (x - 1, y - 1), (x, y - 1), (x + 1, y - 1), \
+                  (x - 1, y), (x + 1, y), (x - 1, y + 1), \
+                  (x, y + 1), (x + 1, y + 1):
         if 0 <= nx < w and 0 <= ny < h:
             neighbors.append([nx, ny])
     return neighbors
 
 def part1():
-    rows, part_numbers, part_symbols = parseInput(3)
-
-    h = len(rows)
-    w = len(rows[0])
+    rows, w, h, part_numbers, part_symbols, part_numbers_dict = parseInput(3)
 
     nums_to_add = set()
     # for each symbol, check its neighbors for part_numbers
     for part_symbol in part_symbols:
-        print(part_symbol)
+        # print(part_symbol)
         for nx, ny in get_neighbors(part_symbol.coord[0], part_symbol.coord[1], w, h):
             if rows[ny][nx].isdecimal():
                 # lookup the coords in the part_numbers
-                for part_number in part_numbers:
-                    if (nx, ny) in part_number.coords:
-                        print("adding: ", part_number)
-                        nums_to_add.add(part_number)
+                nums_to_add.add(part_numbers_dict[(nx, ny)])
     answer = 0
     for n in nums_to_add:
-        print(n)
+        # print(n)
         answer += n.value
     print(answer)
 
 
 def part2():
-    rows, part_numbers, part_symbols = parseInput(3)
+    rows, w, h, part_numbers, part_symbols, part_numbers_dict = parseInput(3)
 
-    h = len(rows)
-    w = len(rows[0])
-
-    gear_ratios = []
+    answer = 0
     # for each symbol, check its neighbors for part_numbers
     for part_symbol in part_symbols:
-        print(part_symbol)
+        # print(part_symbol)
         nums_to_add = set()
         for nx, ny in get_neighbors(part_symbol.coord[0], part_symbol.coord[1], w, h):
             if rows[ny][nx].isdecimal():
                 # lookup the coords in the part_numbers
-                for part_number in part_numbers:
-                    if (nx, ny) in part_number.coords:
-                        print("adding: ", part_number)
-                        nums_to_add.add(part_number)
+                nums_to_add.add(part_numbers_dict[(nx, ny)])
         # check if it's a 'gear'
         gear_ratio = 1
         if len(nums_to_add) == 2:
             for n in nums_to_add:
                 gear_ratio = gear_ratio * n.value
-            print(gear_ratio)
-            gear_ratios.append(gear_ratio)
-    print(sum(gear_ratios))
+            answer += gear_ratio
+    print(answer)
 
 if __name__ == "__main__":
     print("\nPART 1 RESULT")
