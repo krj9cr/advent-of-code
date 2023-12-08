3872ad8def628634542cb894528c85ff37e5c08f
krj9cr_uva <krj9cr@virginia.edu>
2023-12-08 00:11:17 -0500

    Day 8 pt 1

--- year_2023/src/Day08.py
+++ year_2023/src/Day08.py
@@ -1,15 +1,59 @@
 import time
 
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return self.value + " = " + "(" + self.left + ", " + self.right + ")"
+
 def parseInput(day):
     dayf = "{:02d}".format(day)
     path = __file__.rstrip(f"Day{dayf}.py") + f"../input/day{dayf}.txt"
     with open(path, 'r') as file:
-        lines = [line.strip() for line in file]
-        return lines
+        i = 0
+        directions = ""
+        nodes = {}
+        for line in file:
+            line = line.strip()
+            if i == 0:
+                directions = line
+            elif i > 1:
+                s1 = line.split(" = ")
+                value = s1[0]
+                s2 = s1[1].strip("(").strip(")").split(", ")
+                left = s2[0]
+                right = s2[1]
+                nodes[value] = Node(value, left, right)
+            i += 1
+        return directions, nodes
 
 def part1():
-    lines = parseInput(8)
-    print(lines)
+    directions, nodes = parseInput(8)
+    print(directions)
+    for node in nodes:
+        print(nodes[node])
+
+    curr_node = nodes["AAA"]
+    i = 0
+    steps = 0
+    while True:
+        print(curr_node)
+        if curr_node.value == "ZZZ":
+            break
+        if i >= len(directions):
+            i = 0
+        next_direction = directions[i]
+        print("going", next_direction)
+        if next_direction == "L":
+            curr_node = nodes[curr_node.left]
+        else:
+            curr_node = nodes[curr_node.right]
+        i += 1
+        steps += 1
+    print("steps", steps)
 
 def part2():
     lines = parseInput(8)
