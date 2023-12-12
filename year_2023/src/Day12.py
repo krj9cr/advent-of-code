0d7fe7d4079ab42923c5179202f001e597caa4fd
krj9cr_uva <krj9cr@virginia.edu>
2023-12-12 00:21:42 -0500

    Day 12 pt 1

--- year_2023/src/Day12.py
+++ year_2023/src/Day12.py
@@ -1,15 +1,83 @@
+import itertools
 import time
+
+class SpringGroup:
+    def __init__(self, springs, arrangement):
+        self.springs = springs
+        self.arrangement = arrangement
+
+    def __str__(self):
+        return self.springs + " " + str(self.arrangement)
+
+    def satisfiesArrangement(self, springs):
+        groups = []
+        currentGroup = ""
+        for char in springs:
+            if char == "#":
+                currentGroup += char
+            else:
+                if currentGroup != "":
+                    groups.append(len(currentGroup))
+                    currentGroup = ""
+        if currentGroup != "":
+            groups.append(len(currentGroup))
+        return groups == self.arrangement
+
+    def guessArrangement(self):
+        # print(self)
+        # how many question marks
+        numQuestions = 0
+        for char in self.springs:
+            if char == "?":
+                numQuestions += 1
+        # print(numQuestions)
+
+        # get all possible combos of filling in the question marks
+        combos = list(itertools.product([".", "#"], repeat=numQuestions))
+        answer = 0
+        for combo in combos:
+            springs = ""
+            c = 0
+            for i in range(len(self.springs)):
+                char = self.springs[i]
+                if char == "?":
+                    springs += combo[c]
+                    c += 1
+                else:
+                    springs += char
+            # print(springs)
+            # check if this satisfies and count it
+            if self.satisfiesArrangement(springs):
+                answer += 1
+        return answer
+
 
 def parseInput(day):
     dayf = "{:02d}".format(day)
     path = __file__.rstrip(f"Day{dayf}.py") + f"../input/day{dayf}.txt"
     with open(path, 'r') as file:
-        lines = [line.strip() for line in file]
-        return lines
+        springGroups = []
+        for line in file:
+            parts = line.strip().split(" ")
+            springs = parts[0]
+            parts2 = parts[1].split(",")
+            arrangement = [int(c) for c in parts2]
+            springGroups.append(SpringGroup(springs, arrangement))
+        return springGroups
 
 def part1():
-    lines = parseInput(12)
-    print(lines)
+    springGroups = parseInput(12)
+    answer = 0
+    for group in springGroups:
+        a = group.guessArrangement()
+        print(group, a)
+        answer += a
+    print(answer)
+
+    # testing
+    # g = SpringGroup("???.###", [1, 1, 3])
+    # print(g.satisfiesArrangement("...#...#.###..."))
+    # print(g.guessArrangement())
 
 def part2():
     lines = parseInput(12)
