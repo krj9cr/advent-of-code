6dcb9caca389195f39c7244afbd3fb74f3cfc553
Karen Craig <karen.craig@ccri.com>
2023-12-13 21:33:00 +0000

    Some work on pt 2

--- year_2023/src/Day13.py
+++ year_2023/src/Day13.py
@@ -57,6 +57,60 @@ def find_horizontal_mirrors(grid):
                 break
     return value
 
+# check if there's exactly one difference between 2 rows, and return its index and which row has the "."
+def compare_rows_smudge(row1, row2):
+    diff = 0
+    diffIdx = None
+    whichRow = None
+    for i in range(len(row1)):
+        char1 = row1[i]
+        char2 = row2[i]
+        if char1 != char2:
+            diff += 1
+            diffIdx = i
+            if char1 == ".":
+                whichRow = 1
+            else:
+                whichRow = 2
+    if diff == 1:
+        return diffIdx, whichRow
+    return None
+
+def find_smudge(grid):
+    value = None
+    for j in range(len(grid)-1):
+        row1 = grid[j]
+        row2 = grid[j+1]
+        # TODO: need to check if different by 1 here, too
+        if (row1 == row2).all():
+            print("horitzontal mirror at:", j, j +1)
+            # check all the other pairs
+            upper = j
+            lower = j + 1
+            mirrored = True
+            while True:
+                upper -= 1
+                lower += 1
+                if upper < 0 or lower >= len(grid):
+                    break
+                row1 = grid[upper]
+                row2 = grid[lower]
+                print("comparing", upper, lower)
+                print(row1)
+                print(row2)
+                if (row1 == row2).all():
+                    print("equal")
+                else:
+                    # check if rows are different by one
+                    # try making a new grid and run "part1" again on it to see if we get a different value than we have in "part1Answers"
+                    mirrored = False
+                    print("not equal")
+                    break
+            if mirrored:
+                value = j + 1
+                break
+    return value
+
 def part1():
     grids = parseInput(13)
     for grid in grids:
@@ -82,23 +136,61 @@ def part1():
         print()
         answer += value
     print(answer)
-
-# 22376 too low
         
 
 def part2():
-    lines = parseInput(13)
-    print(lines)
+    grids = parseInput(13)
+    for grid in grids:
+        print_2d_grid(grid)
+        print()
+
+    # run pt 1 and save answers in a dict so we make sure to get a different answer
+    part1Answers = {}
+    for gid in range(len(grids)):
+        grid = grids[gid]
+        value = None
+        print_2d_grid(grid)
+        print("finding vertical mirrors")
+        rotatedGrid = np.rot90(grid, k=3, axes=(0,1))
+        print("rotated")
+        print_2d_grid(rotatedGrid)
+        value = find_horizontal_mirrors(rotatedGrid)
+        print("vertical mirror at:", value)
+
+        if not value:
+            print("finding horizontal mirrors")
+            print_2d_grid(grid)
+            value = find_horizontal_mirrors(grid) * 100
+            print("horiztonal mirror at:", value)
+        print()
+        part1Answers[gid] = value
+    print(part1Answers)
+
+    # for each grid
+    for gid in range(len(grids)):
+        grid = grids[gid]
+        print_2d_grid(grid)
+        # find each dot
+        for j in range(len(grid)):
+            row = grid[j]
+            for i in range(len(row)):
+                char = row[i]
+                if char == ".":
+                    print(i,j)
+                    # try changing each dot, and see if changing it still matches another line
+                    # if it does match another line, run "find_horizontal_mirrors" with that grid, check that it's a different answer from "part1Answers"
+        print()
+
 
 if __name__ == "__main__":
-    print("\nPART 1 RESULT")
-    start = time.perf_counter()
-    part1()
-    end = time.perf_counter()
-    print("Time (ms):", (end - start) * 1000)
-
-    # print("\nPART 2 RESULT")
+    # print("\nPART 1 RESULT")
     # start = time.perf_counter()
-    # part2()
+    # part1()
     # end = time.perf_counter()
     # print("Time (ms):", (end - start) * 1000)
+
+    print("\nPART 2 RESULT")
+    start = time.perf_counter()
+    part2()
+    end = time.perf_counter()
+    print("Time (ms):", (end - start) * 1000)
