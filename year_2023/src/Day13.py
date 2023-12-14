37545827731c38e9aadae6c74415005f9aac051e
krj9cr_uva <krj9cr@virginia.edu>
2023-12-13 20:56:09 -0500

    pt 2 but wrong answer

--- year_2023/src/Day13.py
+++ year_2023/src/Day13.py
@@ -1,3 +1,4 @@
+import copy
 import time
 import numpy as np
 
@@ -25,13 +26,18 @@ def print_2d_grid(grid):
             print(item, end="")
         print()
 
-def find_horizontal_mirrors(grid):
+def find_mirror_value(grid, vertical=True, notValue=None):
     value = None
     for j in range(len(grid)-1):
         row1 = grid[j]
         row2 = grid[j+1]
         if (row1 == row2).all():
-            print("horitzontal mirror at:", j, j +1)
+            # if vertical:
+            #     print("vertical ", end="")
+            # else:
+            #     print("horizonal ", end="")
+            # print("mirror at:", j, j +1)
+            # print_2d_grid(grid)
             # check all the other pairs
             upper = j
             lower = j + 1
@@ -43,144 +49,110 @@ def find_horizontal_mirrors(grid):
                     break
                 row1 = grid[upper]
                 row2 = grid[lower]
-                print("comparing", upper, lower)
-                print(row1)
-                print(row2)
+                # print("comparing", upper, lower)
+                # print(row1)
+                # print(row2)
                 if (row1 == row2).all():
-                    print("equal")
+                    # print("equal")
+                    True  # do nothing
                 else:
                     mirrored = False
-                    print("not equal")
+                    # print("not equal")
                     break
             if mirrored:
                 value = j + 1
-                break
+                if not vertical:
+                    value *= 100
+                # print(value)
+                # check if we want to search for a different value
+                if notValue and value != notValue:
+                    # print("unique value", value, "!=", notValue)
+                    break
+                else:
+                    # print("continuing")
+                    continue
     return value
 
-# check if there's exactly one difference between 2 rows, and return its index and which row has the "."
-def compare_rows_smudge(row1, row2):
-    diff = 0
-    diffIdx = None
-    whichRow = None
-    for i in range(len(row1)):
-        char1 = row1[i]
-        char2 = row2[i]
-        if char1 != char2:
-            diff += 1
-            diffIdx = i
-            if char1 == ".":
-                whichRow = 1
-            else:
-                whichRow = 2
-    if diff == 1:
-        return diffIdx, whichRow
-    return None
+def find_mirror(grid, notValue=None):
+    # print_2d_grid(grid)
+    # print("finding horizontal mirrors")
+    # print_2d_grid(grid)
+    value = find_mirror_value(grid, vertical=False)
+    # print("horiztonal mirror at:", value)
 
-def find_smudge(grid):
-    value = None
-    for j in range(len(grid)-1):
-        row1 = grid[j]
-        row2 = grid[j+1]
-        # TODO: need to check if different by 1 here, too
-        if (row1 == row2).all():
-            print("horitzontal mirror at:", j, j +1)
-            # check all the other pairs
-            upper = j
-            lower = j + 1
-            mirrored = True
-            while True:
-                upper -= 1
-                lower += 1
-                if upper < 0 or lower >= len(grid):
-                    break
-                row1 = grid[upper]
-                row2 = grid[lower]
-                print("comparing", upper, lower)
-                print(row1)
-                print(row2)
-                if (row1 == row2).all():
-                    print("equal")
-                else:
-                    # check if rows are different by one
-                    # try making a new grid and run "part1" again on it to see if we get a different value than we have in "part1Answers"
-                    mirrored = False
-                    print("not equal")
-                    break
-            if mirrored:
-                value = j + 1
-                break
+    if not value or (notValue and value == notValue):
+        # print("finding vertical mirrors")
+        rotatedGrid = np.rot90(grid, k=3, axes=(0, 1))
+        # print("rotated")
+        # print_2d_grid(rotatedGrid)
+        value = find_mirror_value(rotatedGrid, vertical=True)
+        # print("vertical mirror at:", value)
+    # print()
     return value
 
 def part1():
     grids = parseInput(13)
-    for grid in grids:
-        print_2d_grid(grid)
-        print()
+    # for grid in grids:
+    #     print_2d_grid(grid)
+    #     print()
 
     answer = 0
     for grid in grids:
-        value = None
-        print_2d_grid(grid)
-        print("finding vertical mirrors")
-        rotatedGrid = np.rot90(grid, k=3, axes=(0,1))
-        print("rotated")
-        print_2d_grid(rotatedGrid)
-        value = find_horizontal_mirrors(rotatedGrid)
-        print("vertical mirror at:", value)
-
-        if not value:
-            print("finding horizontal mirrors")
-            print_2d_grid(grid)
-            value = find_horizontal_mirrors(grid) * 100
-            print("horiztonal mirror at:", value)
-        print()
+        value = find_mirror(grid)
         answer += value
     print(answer)
         
 
 def part2():
     grids = parseInput(13)
-    for grid in grids:
-        print_2d_grid(grid)
-        print()
+    answer = 0
 
-    # run pt 1 and save answers in a dict so we make sure to get a different answer
+    # run pt 1 and save answers in a dict, so we make sure to get a different answer
     part1Answers = {}
     for gid in range(len(grids)):
         grid = grids[gid]
-        value = None
-        print_2d_grid(grid)
-        print("finding vertical mirrors")
-        rotatedGrid = np.rot90(grid, k=3, axes=(0,1))
-        print("rotated")
-        print_2d_grid(rotatedGrid)
-        value = find_horizontal_mirrors(rotatedGrid)
-        print("vertical mirror at:", value)
-
-        if not value:
-            print("finding horizontal mirrors")
-            print_2d_grid(grid)
-            value = find_horizontal_mirrors(grid) * 100
-            print("horiztonal mirror at:", value)
-        print()
+        value = find_mirror(grid)
         part1Answers[gid] = value
     print(part1Answers)
 
     # for each grid
     for gid in range(len(grids)):
         grid = grids[gid]
-        print_2d_grid(grid)
-        # find each dot
+        gridValue = part1Answers[gid]
+        # rotatedGrid = np.rot90(grid, k=3, axes=(0, 1))
+        # print_2d_grid(grid)
+        foundAnswer = False
+        # for each position
         for j in range(len(grid)):
             row = grid[j]
             for i in range(len(row)):
+                # print(i, j)
                 char = row[i]
                 if char == ".":
-                    print(i,j)
-                    # try changing each dot, and see if changing it still matches another line
-                    # if it does match another line, run "find_horizontal_mirrors" with that grid, check that it's a different answer from "part1Answers"
-        print()
+                    newChar = "#"
+                else:
+                    newChar = "."
 
+                # run "find_horizontal_mirrors" with that grid
+                newGrid = copy.deepcopy(grid)
+                newGrid[j][i] = newChar
+                # print_2d_grid(newGrid)
+
+                # check that it's a different answer from "part1Answers"
+                value = find_mirror(newGrid, notValue=gridValue)
+                # print(value)
+                if value and value != gridValue:
+                    print_2d_grid(grid)
+                    print("prev answer", gridValue, "found answer", value, "changing", i, j, "to", newChar)
+                    answer += value
+                    foundAnswer = True
+                    break
+            if foundAnswer:
+                break
+        print()
+    print(answer)
+
+# 21882 too low
 
 if __name__ == "__main__":
     # print("\nPART 1 RESULT")
