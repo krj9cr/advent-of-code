776f663059f692d25d7f3976e48570c8a809c4f3
krj9cr_uva <krj9cr@virginia.edu>
2023-12-13 22:43:29 -0500

    Handle multiple values and cleanup

--- year_2023/src/Day13.py
+++ year_2023/src/Day13.py
@@ -27,7 +27,7 @@ def print_2d_grid(grid):
         print()
 
 def find_mirror_value(grid, vertical=True, notValue=None):
-    value = None
+    values = set()
     for j in range(len(grid)-1):
         row1 = grid[j]
         row2 = grid[j+1]
@@ -60,35 +60,33 @@ def find_mirror_value(grid, vertical=True, notValue=None):
                     # print("not equal")
                     break
             if mirrored:
+                # if value and value != notValue:
+                #     print("found another value", end="")
                 value = j + 1
                 if not vertical:
                     value *= 100
                 # print(value)
-                # check if we want to search for a different value
-                if notValue and value != notValue:
-                    # print("unique value", value, "!=", notValue)
-                    break
-                else:
-                    # print("continuing")
-                    continue
-    return value
+                values.add(value)
+
+    return values
 
 def find_mirror(grid, notValue=None):
     # print_2d_grid(grid)
     # print("finding horizontal mirrors")
     # print_2d_grid(grid)
-    value = find_mirror_value(grid, vertical=False)
+    values = find_mirror_value(grid, vertical=False)
     # print("horiztonal mirror at:", value)
 
-    if not value or (notValue and value == notValue):
-        # print("finding vertical mirrors")
-        rotatedGrid = np.rot90(grid, k=3, axes=(0, 1))
-        # print("rotated")
-        # print_2d_grid(rotatedGrid)
-        value = find_mirror_value(rotatedGrid, vertical=True)
-        # print("vertical mirror at:", value)
-    # print()
-    return value
+    # if not value or (notValue and value == notValue):
+    # print("finding vertical mirrors")
+    rotatedGrid = np.rot90(grid, k=3, axes=(0, 1))
+    # print("rotated")
+    # print_2d_grid(rotatedGrid)
+    values2 = find_mirror_value(rotatedGrid, vertical=True)
+
+    values = values.union(values2)
+    # print(values)
+    return values
 
 def part1():
     grids = parseInput(13)
@@ -98,7 +96,7 @@ def part1():
 
     answer = 0
     for grid in grids:
-        value = find_mirror(grid)
+        value = find_mirror(grid).pop()
         answer += value
     print(answer)
         
@@ -111,9 +109,9 @@ def part2():
     part1Answers = {}
     for gid in range(len(grids)):
         grid = grids[gid]
-        value = find_mirror(grid)
+        value = find_mirror(grid).pop()
         part1Answers[gid] = value
-    print(part1Answers)
+    # print(part1Answers)
 
     # for each grid
     for gid in range(len(grids)):
@@ -139,27 +137,31 @@ def part2():
                 # print_2d_grid(newGrid)
 
                 # check that it's a different answer from "part1Answers"
-                value = find_mirror(newGrid, notValue=gridValue)
-                # print(value)
-                if value and value != gridValue:
-                    print_2d_grid(grid)
-                    print("prev answer", gridValue, "found answer", value, "changing", i, j, "to", newChar)
+                values = find_mirror(newGrid, notValue=gridValue)
+                try:
+                    values.remove(gridValue)
+                except:
+                    True  # do nothing
+                if len(values) > 1:
+                    print("found multiple answers")
+                elif len(values) > 0:
+                    value = values.pop()
+                    # print_2d_grid(grid)
+                    # print("prev answer", gridValue, "found answer", value, "changing", i, j, "to", newChar)
                     answer += value
                     foundAnswer = True
                     break
             if foundAnswer:
                 break
-        print()
+        # print()
     print(answer)
 
-# 21882 too low
-
 if __name__ == "__main__":
-    # print("\nPART 1 RESULT")
-    # start = time.perf_counter()
-    # part1()
-    # end = time.perf_counter()
-    # print("Time (ms):", (end - start) * 1000)
+    print("\nPART 1 RESULT")
+    start = time.perf_counter()
+    part1()
+    end = time.perf_counter()
+    print("Time (ms):", (end - start) * 1000)
 
     print("\nPART 2 RESULT")
     start = time.perf_counter()
