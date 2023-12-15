6816ce0ccfc3ca607d692f6dc2138064a1c5a8e3
krj9cr_uva <krj9cr@virginia.edu>
2023-12-15 00:06:13 -0500

    Dday 15 pt 1

--- year_2023/src/Day15.py
+++ year_2023/src/Day15.py
@@ -4,16 +4,33 @@ def parseInput(day):
     dayf = "{:02d}".format(day)
     path = __file__.rstrip(f"Day{dayf}.py") + f"../input/day{dayf}.txt"
     with open(path, 'r') as file:
-        lines = [line.strip() for line in file]
-        return lines
+        line = [line.strip() for line in file][0]
+        return line.split(',')
+
+def HASH(string):
+    curr = 0
+    for char in string:
+        ascii_code = ord(char)
+        print("ascii_code", ascii_code)
+        curr += ascii_code
+        print("curr", curr)
+        curr *= 17
+        curr %= 256
+    return curr
 
 def part1():
-    lines = parseInput(15)
-    print(lines)
+    steps = parseInput(15)
+    print(steps)
+
+    answer = 0
+    for step in steps:
+        print(step)
+        answer += HASH(step)
+    print(answer)
+
 
 def part2():
-    lines = parseInput(15)
-    print(lines)
+    True
 
 if __name__ == "__main__":
     print("\nPART 1 RESULT")
