185a3b932492684b19641af6b56ee3ef4a010584
krj9cr_uva <krj9cr@virginia.edu>
2023-12-15 00:30:23 -0500

    Day 15 pt 2

--- year_2023/src/Day15.py
+++ year_2023/src/Day15.py
@@ -1,4 +1,5 @@
 import time
+from collections import OrderedDict
 
 def parseInput(day):
     dayf = "{:02d}".format(day)
@@ -11,9 +12,9 @@ def HASH(string):
     curr = 0
     for char in string:
         ascii_code = ord(char)
-        print("ascii_code", ascii_code)
+        # print("ascii_code", ascii_code)
         curr += ascii_code
-        print("curr", curr)
+        # print("curr", curr)
         curr *= 17
         curr %= 256
     return curr
@@ -30,17 +31,65 @@ def part1():
 
 
 def part2():
-    True
+    steps = parseInput(15)
+    print(steps)
+
+    # init boxes
+    boxes = {}
+    for i in range(256):
+        boxes[i] = OrderedDict()
+
+    for step in steps:
+        if "-" in step:
+            label = step.strip("-")
+            boxId = HASH(label)
+            box = boxes[boxId]
+            if label in box:
+                del box[label]
+        elif "=" in step:
+            splitStep = step.split("=")
+            label = splitStep[0]
+            focalLength = splitStep[1]
+            boxId = HASH(label)
+            box = boxes[boxId]
+            if label in box:
+                # replace lens
+                box[label] = focalLength
+            else:
+                box[label] = focalLength
+            boxes[boxId] = box
+        # # print (for debugging)
+        # print(step, "boxId", boxId)
+        # for boxId in boxes:
+        #     box = boxes[boxId]
+        #     if len(box) > 0:
+        #         print("Box", boxId, box)
+
+    answer = 0
+    for boxId in boxes:
+        box = boxes[boxId]
+        if len(box) > 0:
+            print("Box", boxId, box)
+        i = 0
+        while i < len(box):
+            label = list(box)[i]
+            focus = box[label]
+            print(label, focus)
+            a = (1 + boxId) * (i + 1) * int(focus)
+            print((1 + boxId),  (i + 1), int(focus), "=", a)
+            answer += a
+            i += 1
+    print(answer)
 
 if __name__ == "__main__":
-    print("\nPART 1 RESULT")
-    start = time.perf_counter()
-    part1()
-    end = time.perf_counter()
-    print("Time (ms):", (end - start) * 1000)
-
-    # print("\nPART 2 RESULT")
+    # print("\nPART 1 RESULT")
     # start = time.perf_counter()
-    # part2()
+    # part1()
     # end = time.perf_counter()
     # print("Time (ms):", (end - start) * 1000)
+
+    print("\nPART 2 RESULT")
+    start = time.perf_counter()
+    part2()
+    end = time.perf_counter()
+    print("Time (ms):", (end - start) * 1000)
