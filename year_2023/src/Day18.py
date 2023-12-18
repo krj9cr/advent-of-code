b5aeb8f2463e2fe56f48e27c59aff4648cdcbc1c
krj9cr_uva <krj9cr@virginia.edu>
2023-12-18 00:35:03 -0500

    Start Day 18

--- year_2023/src/Day18.py
+++ year_2023/src/Day18.py
@@ -1,4 +1,5 @@
 import time
+from shapely.geometry import Polygon
 
 def parseInput(day):
     dayf = "{:02d}".format(day)
@@ -7,12 +8,54 @@ def parseInput(day):
         lines = []
         for line in file:
             line = line.strip()
-            lines.append(line)
+            parts = line.split(" ")
+            lines.append((parts[0], int(parts[1]), parts[2]))
         return lines
 
 def part1():
-    lines = parseInput(18)
-    print(lines)
+    digs = parseInput(18)
+    print(digs)
+
+    startPos = (0, 0)
+    locations = {startPos: 0}
+    vertices = [startPos]
+    x, y = startPos
+    for (direction, steps, _) in digs:
+        if direction == "R":
+            for i in range(steps + 1):
+                locations[(x + i, y)] = 0
+            x, y = x + steps, y
+            vertices.append((x, y))
+        elif direction == "L":
+            for i in range(steps + 1):
+                locations[(x - i, y)] = 0
+            x, y = x - steps, y
+            vertices.append((x, y))
+        elif direction == "U":
+            for i in range(steps + 1):
+                locations[(x, y - i)] = 0
+            x, y = x, y - steps
+            vertices.append((x, y))
+        elif direction == "D":
+            for i in range(steps + 1):
+                locations[(x, y + i)] = 0
+            x, y = x, y + steps
+            vertices.append((x, y))
+    points = list(locations.keys()) + [startPos]
+    print(points)
+    print("vertices", vertices[:-1])
+
+    # this is... not working for concave polygon
+    pgon = Polygon(points)
+    print(pgon.area)
+
+    # idea:
+    # find the minX/maxX and Y of all the vertices
+    # for each spot in the grid
+    #   if it's in the list of points, count it
+    #   otherwise try to shoot out in each direction, it should hit a polygon point in all 4 directions
+    #   if it goes out of bounds, we know it's not contained
+    #   count the contained points
 
 def part2():
     lines = parseInput(18)
