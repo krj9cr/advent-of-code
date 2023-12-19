c125f5b3e9f461407a3498bf524d11a3a79e4a87
krj9cr_uva <krj9cr@virginia.edu>
2023-12-18 22:26:24 -0500

    Day 18 pt 1

--- year_2023/src/Day18.py
+++ year_2023/src/Day18.py
@@ -1,5 +1,5 @@
 import time
-from shapely.geometry import Polygon
+from collections import deque
 
 def parseInput(day):
     dayf = "{:02d}".format(day)
@@ -11,6 +11,19 @@ def parseInput(day):
             parts = line.split(" ")
             lines.append((parts[0], int(parts[1]), parts[2]))
         return lines
+
+def bfs_2d_grid(start, points, minX, maxX, minY, maxY):
+    queue = deque([start])
+    seen = set([start])
+    while queue:
+        (x, y) = queue.popleft()
+        for x2, y2 in ((x, y - 1), (x - 1, y), (x + 1, y), (x, y + 1)):
+            if minX <= x2 < maxX and minY <= y2 < maxY:
+                next = (x2, y2)
+                if next not in seen and next not in points:
+                    queue.append(next)
+                    seen.add(next)
+    return seen
 
 def part1():
     digs = parseInput(18)
@@ -41,21 +54,43 @@ def part1():
                 locations[(x, y + i)] = 0
             x, y = x, y + steps
             vertices.append((x, y))
-    points = list(locations.keys()) + [startPos]
+    points = set(list(locations.keys()) + [startPos])
     print(points)
     print("vertices", vertices[:-1])
 
-    # this is... not working for concave polygon
-    pgon = Polygon(points)
-    print(pgon.area)
-
-    # idea:
     # find the minX/maxX and Y of all the vertices
-    # for each spot in the grid
-    #   if it's in the list of points, count it
-    #   otherwise try to shoot out in each direction, it should hit a polygon point in all 4 directions
-    #   if it goes out of bounds, we know it's not contained
-    #   count the contained points
+    minX = minY = 99999
+    maxX = maxY = 0
+    for (x, y) in vertices:
+        if x < minX:
+            minX = x
+        if x > maxX:
+            maxX = x
+        if y < minY:
+            minY = y
+        if y > maxY:
+            maxY = y
+    print("mins", minX, minY, "maxes", maxX, maxY)
+
+    # try to print the thing
+    # for j in range(minY, maxY + 1):
+    #     for i in range(minX, maxX + 1):
+    #         if (i, j) in points:
+    #             print("#", end="")
+    #         else:
+    #             print(".", end="")
+    #     print()
+
+    # we're going to "cheat" and just pick a point we know is inside and do a flood fill with bfs
+    # find some points near the top of the grid
+    # for x, y in points:
+    #     if y == minY:
+    #         print("minY point", x, y)
+    # inside = (1, 1)  # example case
+    inside = (135, -77)  # my input
+    seen = bfs_2d_grid(inside, points, minX, maxX, minY, maxY)
+    print(len(seen))
+    print(len(seen) + len(points))
 
 def part2():
     lines = parseInput(18)
