f12464069dc51986ad72414e927c4354635544cf
krj9cr_uva <krj9cr@virginia.edu>
2023-12-28 23:14:43 -0500

    Day 21 pt 2

--- year_2023/src/Day21.py
+++ year_2023/src/Day21.py
@@ -4,6 +4,7 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy
+from Day09 import processSequence, allSame
 
 def parseInput(day):
     dayf = "{:02d}".format(day)
@@ -39,10 +40,10 @@ def part1():
                 if nextPos not in rocks:
                     elf_locations2.add(nextPos)
         elf_locations = elf_locations2
-        print(elf_locations)
+        # print(elf_locations)
     print(len(elf_locations))
 
-def get_tile(pos, w, h):
+def pos_to_tile(pos, w, h):
     x, y = pos
     x2 = math.floor(x / w)
     y2 = math.floor(y / h)
@@ -55,9 +56,32 @@ def rock_to_tile(rock, tile, w, h):
     y = r2 + (t2 * h)
     return x, y
 
-def print_center_tile(elf_locations, rocks, w, h):
-    for j in range(h):
-        for i in range(w):
+def tile_first_coord(tile, w, h):
+    return tile[0] * w, tile[1] * h
+
+def print_tiles(elf_locations, rocks, tiles, w, h):
+    # find the mins/maxes
+    minX = 0
+    maxX = w
+    minY = 0
+    maxY = h
+    tile_coords = []
+    for tile in tiles:
+        x, y = tile_first_coord(tile, w, h)
+        tile_coords.append((x, y))
+        if x < minX:
+            minX = x
+        if x + w > maxX:
+            maxX = x
+        if y < minY:
+            minY = y
+        if y + h > maxY:
+            maxY = y
+
+    print("tile_coords:", tile_coords)
+    print("x", minX, maxX, "y:", minY, maxY)
+    for j in range(minY, maxY):
+        for i in range(minX, maxX):
             if (i, j) in elf_locations:
                 print("O", end="")
             elif (i, j) in rocks:
@@ -66,16 +90,6 @@ def print_center_tile(elf_locations, rocks, w, h):
                 print(".", end="")
         print()
 
-def print_area(elf_locations, rocks, minX, maxX, minY, maxY):
-    for j in range(minX, maxX):
-        for i in range(minY, maxY):
-            if (i, j) in elf_locations:
-                print("O", end="")
-            elif (i, j) in rocks:
-                print("#", end="")
-            else:
-                print(".", end="")
-        print()
 
 # insight:
 # once the center tile gets filled out, spots "alternate" between on and off
@@ -83,31 +97,27 @@ def print_area(elf_locations, rocks, minX, maxX, minY, maxY):
 def part2():
     startPos, rocks, w, h = parseInput(21)
     # print(startPos, rocks)
-    # print(w, h)
+    print("w/h:", w, h)
 
-    # takes 20 minutes to do 1000
-    num_steps = 30
+    # takes about 20 minutes to do 1000
+    num_steps = 65 + (2*131)
 
     orig_rocks = copy.deepcopy(rocks)
     seen_tiles = set()
 
     elf_locations = {startPos}
-    minX = minY = 0
-    maxX = w
-    maxY = h
+    history = []
     # steps_plt = []
     # step_counts = []
-    for step in range(num_steps):
-        if step % 1 == 0:
-            print("step", step)
-            # print("elf locations", len(elf_locations))
-            # print(elf_locations)
-            # steps_plt.append(step)
-            # step_counts.append(len(elf_locations))
-            # print_center_tile(elf_locations, rocks, w, h)
-            print_area(elf_locations, rocks, minX, maxX, minY, maxY)
-        print(seen_tiles)
-        print()
+    for step in range(num_steps+1):
+        if step == 65 or step == 65 + 131 or step == 65 + (2*131):
+            num_elf_locations = len(elf_locations)
+            print("step", step, "elf_locations", num_elf_locations)
+            history.append(num_elf_locations)
+            # print_area(elf_locations, rocks, minX, maxX, minY, maxY)
+            # print_tiles(elf_locations, rocks, seen_tiles, w, h)
+        # print(seen_tiles)
+        # print()
         elf_locations2 = set()
         for loc in elf_locations:
             x, y = loc
@@ -115,7 +125,7 @@ def part2():
                 nextPos = (x2, y2)
                 # idea: compute where the rocks are dynamically?
                 # figure out what tile we're in
-                tile = get_tile(loc, w, h)
+                tile = pos_to_tile(loc, w, h)
                 if tile not in seen_tiles:
                     # get all that tiles rocks
                     for rock in orig_rocks:
@@ -125,24 +135,34 @@ def part2():
                 # then check if this pos is in that list of rocks
                 if nextPos not in rocks:
                     elf_locations2.add(nextPos)
-                    # update mins
-                    if x2 < minX:
-                        minX = x
-                    if x2 > maxX:
-                        maxX = x
-                    if y2 < minY:
-                        minY = y2
-                    if y2 > maxY:
-                        maxY = y2
         elf_locations = elf_locations2
-        # print(elf_locations)
+
+    # read a hint to use Day09 stuff to predict next numbers
+    # history = [3955, 35214, 97607]
+    # since 202300 * 131 + 65 = 26501365, we generate a sequence of numbers that fits
+    for k in range(202300):
+        sequences = [history]
+        # generate all the sequences until we end up with one with the same number
+        while True:
+            sequences.append(processSequence(sequences[-1]))
+            if allSame(sequences[-1]):
+                break
+        # print(sequences)
+        num = sequences[-1][0]
+        for i in range(len(sequences) - 2, -1, -1):
+            sequence = sequences[i]
+            num = sequence[-1] + num
+        # remove one from the front otherwise this will increasingly take forever
+        history = history[1:] + [num]
+        if k == 202297:  # we use this number because we already computed 3 values in the sequence
+            print(k, ":", num)
 
 if __name__ == "__main__":
-    # print("\nPART 1 RESULT")
-    # start = time.perf_counter()
-    # part1()
-    # end = time.perf_counter()
-    # print("Time (ms):", (end - start) * 1000)
+    print("\nPART 1 RESULT")
+    start = time.perf_counter()
+    part1()
+    end = time.perf_counter()
+    print("Time (ms):", (end - start) * 1000)
 
     print("\nPART 2 RESULT")
     start = time.perf_counter()
--- year_2023/src/test_Day21.py
+++ year_2023/src/test_Day21.py
@@ -1,22 +1,22 @@
 # Note: to run this in PyCharm, set PYTHONENV=. as an environment variable,
 #       which adds the current directory to be able to import Day21
-from Day21 import get_tile, rock_to_tile
+from Day21 import pos_to_tile, rock_to_tile
 
 def test_get_tile():
-    assert get_tile((0, 0), 11, 11) == (0, 0)
-    assert get_tile((1, 1), 11, 11) == (0, 0)
-    assert get_tile((1, 11), 11, 11) == (0, 1)
-    assert get_tile((1, 15), 11, 11) == (0, 1)
-    assert get_tile((1, 21), 11, 11) == (0, 1)
-    assert get_tile((1, 22), 11, 11) == (0, 2)
-    assert get_tile((11, 11), 11, 11) == (1, 1)
-    assert get_tile((12, 12), 11, 11) == (1, 1)
-    assert get_tile((12, 10), 11, 11) == (1, 0)
-    assert get_tile((-1, 0), 11, 11) == (-1, 0)
-    assert get_tile((-11, 0), 11, 11) == (-1, 0)
-    assert get_tile((-12, 0), 11, 11) == (-2, 0)
-    assert get_tile((0, -1), 11, 11) == (0, -1)
-    assert get_tile((-1, -1), 11, 11) == (-1, -1)
+    assert pos_to_tile((0, 0), 11, 11) == (0, 0)
+    assert pos_to_tile((1, 1), 11, 11) == (0, 0)
+    assert pos_to_tile((1, 11), 11, 11) == (0, 1)
+    assert pos_to_tile((1, 15), 11, 11) == (0, 1)
+    assert pos_to_tile((1, 21), 11, 11) == (0, 1)
+    assert pos_to_tile((1, 22), 11, 11) == (0, 2)
+    assert pos_to_tile((11, 11), 11, 11) == (1, 1)
+    assert pos_to_tile((12, 12), 11, 11) == (1, 1)
+    assert pos_to_tile((12, 10), 11, 11) == (1, 0)
+    assert pos_to_tile((-1, 0), 11, 11) == (-1, 0)
+    assert pos_to_tile((-11, 0), 11, 11) == (-1, 0)
+    assert pos_to_tile((-12, 0), 11, 11) == (-2, 0)
+    assert pos_to_tile((0, -1), 11, 11) == (0, -1)
+    assert pos_to_tile((-1, -1), 11, 11) == (-1, -1)
 
 def test_rock_to_tile():
     assert rock_to_tile((0, 0), (0, 0), 11, 11) == (0, 0)
