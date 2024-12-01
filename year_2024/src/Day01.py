ed3dd74f5383174959d0979206915fe0653ddb17
krj9cr_uva <krj9cr@virginia.edu>
2024-12-01 08:14:32 -0500

    Day 1

--- year_2024/src/Day01.py
+++ year_2024/src/Day01.py
@@ -1,23 +1,50 @@
 import time
+from collections import Counter
 
 def parseInput(day):
     dayf = "{:02d}".format(day)
     path = __file__.rstrip(f"Day{dayf}.py") + f"../input/day{dayf}.txt"
+    list1 = []
+    list2 = []
     with open(path, 'r') as file:
         lines = [line.strip() for line in file]
-        return lines
+        for line in lines:
+            line = line.split(" ")
+            # print(line)
+            item1 = int(line[0])
+            list1.append(item1)
+            item2 = int(line[-1])
+            list2.append(item2)
+        return list1, list2
 
 def part1():
-    lines = parseInput(1)
+    list1, list2 = parseInput(1)
 
-    for line in lines:
-        print(line)
+    list1.sort()
+    list2.sort()
+
+    total = 0
+    for i in range(0, len(list1)):
+        # print(list1[i], list2[i])
+        diff = abs(list1[i]-list2[i])
+        # print("diff", diff)
+        total += diff
+    print("total", total)
 
 def part2():
-    lines = parseInput(1)
+    list1, list2 = parseInput(1)
 
-    for line in lines:
-        print(line)
+    counts = Counter(list2)
+    # print(counts)
+
+    total = 0
+    for i in range(0, len(list1)):
+        # print(list1[i], list2[i])
+        count = counts.get(list1[i])
+        if count:
+            # print("count", count)
+            total += list1[i] * count
+    print("total", total)
 
 if __name__ == "__main__":
     print("\nPART 1 RESULT")
