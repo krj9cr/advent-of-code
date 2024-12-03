af431acf05e03fca473361b38c574d975d930830
krj9cr_uva <krj9cr@virginia.edu>
2024-12-03 09:18:48 -0500

    Day3

--- year_2024/src/Day03.py
+++ year_2024/src/Day03.py
@@ -1,4 +1,5 @@
 import time
+import re
 
 def parseInput(day):
     dayf = "{:02d}".format(day)
@@ -12,21 +13,53 @@ def parseInput(day):
 
 def part1():
     lines = parseInput(3)
-    print(lines)
+    total = 0
+    for line in lines:
+        matches = re.finditer(r"mul\(([0-9]{0,3}),([0-9]{0,3})\)", line)
+        print(line)
+        for match in matches:
+            # print(str(match))
+            first = int(match.group(1))
+            second = int(match.group(2))
+            # print(first, second)
+            total += (first * second)
+    print(total)
 
 def part2():
     lines = parseInput(3)
-    print(lines)
+    total = 0
+    do = True
+    for line in lines:
+        matches = re.finditer(r"mul\(([0-9]{0,3}),([0-9]{0,3})\)|do\(\)|don't\(\)", line)
+        print(line)
+        for match in matches:
+            print(match.group())
+            try:
+                first = int(match.group(1))
+                second = int(match.group(2))
+                if do:
+                    print("adding", first, second)
+                    total += (first * second)
+            except:
+                # it's a do/don't
+                if match.group() == "do()":
+                    do = True
+                    print("DO")
+                else:
+                    do = False
+                    print("DONT")
+            # print(first, second)
+    print(total)
 
 if __name__ == "__main__":
-    print("\nPART 1 RESULT")
-    start = time.perf_counter()
-    part1()
-    end = time.perf_counter()
-    print("Time (ms):", (end - start) * 1000)
-
-    # print("\nPART 2 RESULT")
+    # print("\nPART 1 RESULT")
     # start = time.perf_counter()
-    # part2()
+    # part1()
     # end = time.perf_counter()
     # print("Time (ms):", (end - start) * 1000)
+
+    print("\nPART 2 RESULT")
+    start = time.perf_counter()
+    part2()
+    end = time.perf_counter()
+    print("Time (ms):", (end - start) * 1000)
