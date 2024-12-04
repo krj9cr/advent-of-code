8bd6b7acc0f9fb468630e6ab2fde016e14a85971
krj9cr_uva <krj9cr@virginia.edu>
2024-12-04 08:58:32 -0500

    Start 4

--- year_2024/src/Day04.py
+++ year_2024/src/Day04.py
@@ -13,9 +13,61 @@ def parseInput(day):
             lines.append(l)
         return lines
 
+def checkBounds(grid, x, y):
+    if 0 <= x < len(grid[0]) and 0 <= y < len(grid):
+        return True
+    return False
+
+def goInDir(grid, dir, x, y, nextLetter):
+    curr = (x, y)
+    (x2, y2) = tuple(map(sum, zip(curr, dir)))
+    if checkBounds(grid, x2, y2):
+        nextItem = grid[y2][x2]
+        print(x2, y2, ":", nextItem)
+        if nextItem == nextLetter:
+            return x2, y2
+
+def checkDir(grid, dir, x, y):
+    next = goInDir(grid, dir, x, y, "M")
+
+
+
 def part1():
     lines = parseInput(4)
     print(lines)
+    count = 0
+    for y in range(0, len(lines)):
+        row = lines[y]
+        for x in range(0, len(row)):
+            item = row[x]
+            if item == "X":
+                print(x, y)
+                # check 8 conditions for directions
+                curr = (x, y)
+                # up
+                dir = (0, -1)
+                (x2, y2) = tuple(map(sum, zip(curr, dir)))
+                if checkBounds(lines, x2, y2):
+                    nextItem = lines[y2][x2]
+                    print(x2, y2, ":", nextItem)
+                    if nextItem == "M":
+
+                # down
+                dir = (0, 1)
+                # left
+                dir = (-1, 0)
+                # right
+                dir = (1, 0)
+                # up left
+                dir = (-1, -1)
+                # up right
+                dir = (1, -1)
+                # down left
+                dir = (-1, 1)
+                # down right
+                dir = (1, 1)
+
+    print(count)
 
 def part2():
     lines = parseInput(4)
