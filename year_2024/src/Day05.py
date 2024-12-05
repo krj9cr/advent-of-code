6b6b12341b3e4d980f193d427afe4d454ba9e5d8
krj9cr_uva <krj9cr@virginia.edu>
2024-12-05 08:39:21 -0500

    Start day 5

--- year_2024/src/Day05.py
+++ year_2024/src/Day05.py
@@ -4,15 +4,57 @@ def parseInput(day):
     dayf = "{:02d}".format(day)
     path = __file__.rstrip(f"Day{dayf}.py") + f"../input/day{dayf}.txt"
     with open(path, 'r') as file:
-        lines = []
+        # rules are kinda backwards
+        # key is a page
+        # values are all the pages that are supposed to come before it
+        rules = {}
+        updates = []
+        processRules = True
         for line in file:
             line = line.strip()
-            lines.append(line)
-        return lines
+            print(line)
+            if line == "":
+                processRules = False
+                continue
+            if processRules:
+                linesplit = line.split("|")
+                key = int(linesplit[1])
+                value = int(linesplit[0])
+                if rules.get(key):
+                    rules[key].add(value)
+                else:
+                    rules[key] = set([value])
+            else:
+                linesplit = line.split(",")
+                updates.append([int(item) for item in linesplit])
+        return rules, updates
 
 def part1():
-    lines = parseInput(5)
-    print(lines)
+    rules, updates = parseInput(5)
+    print(rules)
+    print(updates)
+
+    # for each update
+    update = updates[0]
+    # keep list of seen pages
+    seen = set()
+    # for each item in update
+    for item in update:
+        print("ITEM", item)
+        print("seen: ", seen)
+        # check dict, have we seen any pages in the list? do they intersect?
+        rulePages = rules.get(item)
+        if rulePages:
+            print("rulepages", rulePages)
+            intersection = seen.intersection(rulePages)
+            print("intersection", intersection)
+            if len(intersection) > 0:
+                # rule is violated
+                break
+            else:
+                # rule is good, keep going
+                seen.add(item)
+                continue
 
 def part2():
     lines = parseInput(5)
