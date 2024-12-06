55e43d1571692cb10bd762270da2c1968c59061d
krj9cr_uva <krj9cr@virginia.edu>
2024-12-06 09:11:47 -0500

    Start day 6

--- year_2024/src/Day06.py
+++ year_2024/src/Day06.py
@@ -7,26 +7,115 @@ def parseInput(day):
         lines = []
         for line in file:
             line = line.strip()
-            lines.append(line)
+            l = []
+            for char in line:
+                l.append(char)
+            lines.append(l)
         return lines
 
+def getGuardStart(grid):
+    for j in range(0, len(grid)):
+        row = grid[j]
+        for i in range(0, len(row)):
+            item = row[i]
+            if item == "^":
+                return i, j
+
+def guardStep(grid, guardDir, guardLoc):
+    nextDir = (0, 0)
+    nextGuardDir = ""
+    if guardDir == "^":
+        nextDir = (0, -1)
+        nextGuardDir = ">"
+    elif guardDir == "<":
+        nextDir = (-1, 0)
+        nextGuardDir = "^"
+    elif guardDir == ">":
+        nextDir = (1, 0)
+        nextGuardDir = "v"
+    elif guardDir == "v":
+        nextDir = (0, 1)
+        nextGuardDir = "<"
+    else:
+        print("UH OH")
+    i, j = (guardLoc[0] + nextDir[0], guardLoc[1] + nextDir[1])
+    # check bounds
+    if 0 <= j < len(grid) and 0 <= i < len(grid[j]):
+        nextSpot = grid[j][i]
+        if nextSpot == "#":
+            # don't move, turn
+            return guardLoc, nextGuardDir
+        else:
+            # move forward
+            return (i, j), guardDir
+    else:
+        return None, None
+
+def print_2d_grid(grid, seen):
+    count = 0
+    for j in range(len(grid)):
+        row = grid[j]
+        for i in range(len(row)):
+            item = grid[j][i]
+            if seen.get((i, j)):
+                print("X", end="")
+                count += 1
+            else:
+                print(item, end="")
+        print()
+
+    print("COUNT", count)
+
+# run sim and detect if any loops
+def runSim(grid, guardStart):
+    guardDir = "^"
+    guardLoc = guardStart
+    seen = {guardLoc: guardDir}
+    while True:
+        nextGuardLoc, nextGuardDir = guardStep(grid, guardDir, guardLoc)
+        # guard walked off the map case (no loop)
+        if nextGuardLoc is None or nextGuardDir is None:
+            return False
+        # loop case? we've been at this spot facing the same direction before
+        elif seen.get(nextGuardLoc) and seen[nextGuardLoc] == nextGuardDir:
+            return True
+        else:
+            guardDir = nextGuardDir
+            guardLoc = nextGuardLoc
+            seen[guardLoc] = guardDir
+
 def part1():
-    lines = parseInput(6)
-    print(lines)
+    grid = parseInput(6)
+    guardDir = "^"
+    guardStart = getGuardStart(grid)
+    guardLoc = guardStart
+    seen = {guardLoc: guardDir}
+    while True:
+        print(guardLoc, guardDir)
+        nextGuardLoc, nextGuardDir = guardStep(grid, guardDir, guardLoc)
+        if nextGuardLoc is None or nextGuardDir is None:
+            break
+        else:
+            guardDir = nextGuardDir
+            guardLoc = nextGuardLoc
+            seen[guardLoc] = guardDir
+    print_2d_grid(grid, seen)
+    print(len(seen))
 
 def part2():
-    lines = parseInput(6)
-    print(lines)
+    grid = parseInput(6)
+    guardStart = getGuardStart(grid)
+
 
 if __name__ == "__main__":
-    print("\nPART 1 RESULT")
-    start = time.perf_counter()
-    part1()
-    end = time.perf_counter()
-    print("Time (ms):", (end - start) * 1000)
-
-    # print("\nPART 2 RESULT")
+    # print("\nPART 1 RESULT")
     # start = time.perf_counter()
-    # part2()
+    # part1()
     # end = time.perf_counter()
     # print("Time (ms):", (end - start) * 1000)
+
+    print("\nPART 2 RESULT")
+    start = time.perf_counter()
+    part2()
+    end = time.perf_counter()
+    print("Time (ms):", (end - start) * 1000)
