eb9338641a33815283207bf4e94c7876bd0b1463
krj9cr_uva <krj9cr@virginia.edu>
2024-12-07 09:02:35 -0500

    Day 7

--- year_2024/src/Day07.py
+++ year_2024/src/Day07.py
@@ -1,4 +1,7 @@
+import copy
+import itertools
 import time
+from itertools import combinations_with_replacement
 
 def parseInput(day):
     dayf = "{:02d}".format(day)
@@ -7,26 +10,79 @@ def parseInput(day):
         lines = []
         for line in file:
             line = line.strip()
-            lines.append(line)
+            linesplit = line.split(": ")
+            answer = int(linesplit[0])
+            nums = linesplit[1]
+            numssplit = nums.split(" ")
+            lines.append((answer, [int(n) for n in numssplit]))
         return lines
 
 def part1():
-    lines = parseInput(7)
-    print(lines)
+    equations = parseInput(7)
+    print(equations)
+
+    total = 0
+    for (answer, nums) in equations:
+        print(answer, nums)
+
+        for comb in itertools.product(["+", "*"], repeat=len(nums)-1):
+            print(comb)
+            comb = list(comb)
+            # evaluate
+            agg = nums[0]
+            for i in range(1, len(nums)):
+                n = nums[i]
+                op = comb.pop()
+                if op == "+":
+                    agg = agg + n
+                else:
+                    agg = agg * n
+                print(agg)
+            if answer == agg:
+                print("YAY")
+                total += answer
+                break
+
+    print(total)
 
 def part2():
-    lines = parseInput(7)
-    print(lines)
+    equations = parseInput(7)
+    print(equations)
+
+    total = 0
+    for (answer, nums) in equations:
+        print(answer, nums)
+
+        for comb in itertools.product(["+", "*", "||"], repeat=len(nums)-1):
+            # print(comb)
+            comb = list(comb)
+            nums_comb = copy.deepcopy(nums)
+            # evaluate da rest?
+            while len(nums_comb) > 1:
+                op = comb.pop()
+                if op == "+":
+                    nums_comb[0:2] = [nums_comb[0] + nums_comb[1]]
+                elif op == "*":
+                    nums_comb[0:2] = [nums_comb[0] * nums_comb[1]]
+                else:
+                    nums_comb[0:2] = [int(str(nums_comb[0]) + str(nums_comb[1]))]
+                # print(nums_comb)
+            if answer == nums_comb[0]:
+                print("YAY")
+                total += answer
+                break
+
+    print(total)
 
 if __name__ == "__main__":
-    print("\nPART 1 RESULT")
-    start = time.perf_counter()
-    part1()
-    end = time.perf_counter()
-    print("Time (ms):", (end - start) * 1000)
-
-    # print("\nPART 2 RESULT")
+    # print("\nPART 1 RESULT")
     # start = time.perf_counter()
-    # part2()
+    # part1()
     # end = time.perf_counter()
     # print("Time (ms):", (end - start) * 1000)
+
+    print("\nPART 2 RESULT")
+    start = time.perf_counter()
+    part2()
+    end = time.perf_counter()
+    print("Time (ms):", (end - start) * 1000)
