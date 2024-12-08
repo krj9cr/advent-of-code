b50f689bbc59ece5b745d9ebefee4d87ca56e47e
krj9cr_uva <krj9cr@virginia.edu>
2024-12-08 07:36:16 -0500

    Day 8 part 1

--- year_2024/src/Day08.py
+++ year_2024/src/Day08.py
@@ -1,4 +1,6 @@
 import time
+import itertools
+
 
 def parseInput(day):
     dayf = "{:02d}".format(day)
@@ -7,12 +9,50 @@ def parseInput(day):
         lines = []
         for line in file:
             line = line.strip()
-            lines.append(line)
+            lines.append([char for  char in line])
         return lines
 
 def part1():
-    lines = parseInput(8)
-    print(lines)
+    grid = parseInput(8)
+    w = len(grid[0])
+    h = len(grid)
+    print(grid)
+    print(w, h)
+    antennae = {}
+    for j in range(h):
+        for i in range(w):
+            item = grid[j][i]
+            if item != ".":
+                if antennae.get(item):
+                    antennae[item].append((i, j))
+                else:
+                    antennae[item] = [(i, j)]
+    print(antennae)
+    antinodes = set()
+    # for all the antenna frequencies
+    for frequency in antennae:
+        freq_antennae = antennae[frequency]
+        print(frequency)
+        # for each pair of antennae of the same frequency
+        for antenna1, antenna2 in itertools.combinations(freq_antennae, 2):
+            # compute anti nodes
+            x1, y1 = antenna1[0], antenna1[1]
+            x2, y2 = antenna2[0], antenna2[1]
+            if antenna1[0] > antenna2[0]:
+                x2, y2 = antenna1[0], antenna1[1]
+                x1, y1 = antenna2[0], antenna2[1]
+            rise = (y2 - y1)
+            run = (x2 - x1)
+            # get one point ahead and one behind
+            before1, before2 = x1 - run, y1 - rise
+            after1, after2 = x2 + run, y2 + rise
+            if 0 <= before1 < w and 0 <= before2 < h:
+                antinodes.add((before1, before2))
+            if 0 <= after1 < w and 0 <= after2 < h:
+                antinodes.add((after1, after2))
+            print((x1, y1), (x2, y2), rise, run, (before1, before2), (after1, after2))
+    print(antinodes)
+    print(len(antinodes))
 
 def part2():
     lines = parseInput(8)
