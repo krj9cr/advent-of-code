74d5450a8d8e45033245c047a857dbd63a54abbb
Karen Craig <karen.craig@ga-ccri.com>
2024-12-10 09:16:21 -0500

    Day 10 pt 1

--- year_2024/src/Day10.py
+++ year_2024/src/Day10.py
@@ -1,4 +1,6 @@
+import copy
 import time
+from collections import deque
 
 def parseInput(day):
     dayf = "{:02d}".format(day)
@@ -7,12 +9,69 @@ def parseInput(day):
         lines = []
         for line in file:
             line = line.strip()
-            lines.append(line)
+            row = [int(char) for char in line]
+            lines.append(row)
         return lines
 
+def print_2d_grid(grid, points=None):
+    for j in range(len(grid)):
+        row = grid[j]
+        for i in range(len(row)):
+            if points and (i,j) not in points:
+                print(".",end="")
+            else:
+                item = row[i]
+                print(item, end="")
+        print()
+    print()
+
+def bfs_2d_grid(grid, start, nines):
+    ends = copy.deepcopy(nines)
+    score = 0
+    queue = deque([[start]])
+    seen = set([start])
+    while queue:
+        path = queue.popleft()
+        x, y = path[-1]
+        currentItem = grid[y][x]
+        if (x, y) in ends:
+            ends.remove((x, y))
+            score += 1
+        for x2, y2 in ((x, y - 1), (x - 1, y), (x + 1, y), (x, y + 1)):
+            if 0 <= x2 < len(grid[0]) and 0 <= y2 < len(grid):
+                next = (x2, y2)
+                nextItem = grid[y2][x2]
+                if next not in seen and nextItem - currentItem == 1:
+                    queue.append(path + [next])
+                    seen.add(next)
+    return score, path
+
 def part1():
-    lines = parseInput(10)
-    print(lines)
+    grid = parseInput(10)
+    print_2d_grid(grid)
+
+    nines = set()
+    # find 9's
+    for j in range(len(grid)):
+        row = grid[j]
+        for i in range(len(row)):
+            item = row[i]
+            if item == 9:
+                nines.add((i, j))
+    print(nines)
+
+    # find zeros
+    total = 0
+    for j in range(len(grid)):
+        row = grid[j]
+        for i in range(len(row)):
+            item = row[i]
+            if item == 0:
+                score, path = bfs_2d_grid(grid, (i, j), nines)
+                print((i, j), "SCORE:", score)
+                total += score
+                print_2d_grid(grid, path)
+    print(total)
 
 def part2():
     lines = parseInput(10)
