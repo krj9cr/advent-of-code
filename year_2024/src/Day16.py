9e8f6153de78b3b76581444b0a6ba4ca3304aff8
krj9cr_uva <krj9cr@virginia.edu>
2024-12-16 05:49:29 -0500

    Start day 16

--- year_2024/src/Day16.py
+++ year_2024/src/Day16.py
@@ -1,4 +1,5 @@
 import time
+import heapq
 
 def parseInput(day):
     dayf = "{:02d}".format(day)
@@ -7,12 +8,109 @@ def parseInput(day):
         lines = []
         for line in file:
             line = line.strip()
-            lines.append(line)
+            lines.append([char for char in line])
         return lines
 
+# A custom priority queue used for A Star Search below
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+# guesses the cost of going from current position to goal
+# should always underestimate the actual cost
+def heuristic(goal, x2, y2):
+    (x1, y1) = goal
+    dist = abs(x1 - x2) + abs(y1 - y2)
+    return dist
+
+# See: https://www.redblobgames.com/pathfinding/a-star/implementation.html
+# board  - a 2d array of strings or numbers
+# start  - a tuple of numbers representing the starting coordinate in the 2d grid
+# goal   - a tuple of numbers representing the ending coordinate in the 2d grid
+def a_star_search(board, start, direction, goal):
+    # init
+    queue = PriorityQueue()
+    came_from = {}  # keeps track of our path to the goal
+    cost_so_far = {}  # keeps track of cost to arrive at ((x, y))
+
+    # add start infos
+    queue.put((start[0], start[1]), 0)
+    came_from[(start[0], start[1])] = None
+    cost_so_far[(start[0], start[1])] = 0
+
+    while not queue.empty():
+        x, y = queue.get()
+
+        if (x, y) == goal:
+            break
+
+        # for each adjacent square
+        for x2, y2 in ((x, y + 1), (x + 1, y), (x - 1, y), (x, y - 1)):
+            cost_to_move = 1  # note this could vary on implementation
+            new_cost = cost_so_far[(x, y)] + cost_to_move
+            # if we're not out of bounds
+            if 0 <= y2 < len(board) and 0 <= x2 < len(board[y2]):
+                next_spot = (x2, y2)
+                if board[y2][x2] != "#":
+                    if next_spot not in cost_so_far or new_cost < cost_so_far[next_spot]:
+                        cost_so_far[next_spot] = new_cost
+                        priority = new_cost + heuristic(goal, x2, y2)
+                        queue.put(next_spot, priority)
+                        came_from[next_spot] = (x, y)
+    return came_from, cost_so_far
+
+def print_path(grid, w, h, path):
+    for j in range(h):
+        for i in range(w):
+            item = grid[j][i]
+            if (i, j) in path:
+                print("X",end="")
+            else:
+                print(item, end="")
+        print()
+    print()
+
+
 def part1():
-    lines = parseInput(16)
-    print(lines)
+    grid = parseInput(16)
+
+    h = len(grid)
+    w = len(grid[0])
+    start = None
+    end = None
+    direction = ">"
+    # find start and end
+    for j in range(h):
+        for i in range(w):
+            item = grid[j][i]
+            if item == "S":
+                start = (i, j)
+            elif item == "E":
+                end = (i, j)
+
+    print("start", start)
+    print("end", end)
+
+    came_from, cost_so_far = a_star_search(grid, start, direction, end)
+    print(came_from)
+    # reconstruct path
+    curr = end
+    path = []
+    while curr is not None:
+        path.append(curr)
+        curr = came_from[curr]
+    print(path)
+    print_path(grid, w, h, path)
+    print(cost_so_far[end])
 
 def part2():
     lines = parseInput(16)
