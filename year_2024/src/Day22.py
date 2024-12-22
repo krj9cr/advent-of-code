c8181ac152a1e4959c132dd7ae158aaee91db399
krj9cr_uva <krj9cr@virginia.edu>
2024-12-22 11:14:42 -0500

    Day 22 fix some things

--- year_2024/src/Day22.py
+++ year_2024/src/Day22.py
@@ -17,6 +17,34 @@ def part1():
     total = 0
     for secret in secrets:
         print(secret)
+        for i in range(10):
+            one = secret * 64
+            newSecret = secret ^ one
+            newSecret = newSecret % 16777216
+            two = newSecret // 32
+            newSecret = newSecret ^ two
+            newSecret = newSecret % 16777216
+            three = newSecret * 2048
+            newSecret = newSecret ^ three
+            newSecret = newSecret % 16777216
+            secret = newSecret
+            print(secret)
+        total += secret
+        print()
+    print("Total", total)
+
+def part2():
+    secrets = parseInput(22)
+    print(secrets)
+
+    secret_prices = {}
+    for secret in secrets:
+        print(secret)
+        # get price
+        price = abs(secret) % 10
+        # print(secret, ":", price)
+        secret_prices[secret] = [price]
+        og_secret = secret
         for i in range(2000):
             one = secret * 64
             newSecret = secret ^ one
@@ -26,26 +54,57 @@ def part1():
             newSecret = newSecret % 16777216
             three = newSecret * 2048
             newSecret = newSecret ^ three
-            newSecret = newSecret % 1677721
+            newSecret = newSecret % 16777216
             secret = newSecret
-        print(secret)
-        total += secret
-        print()
+            # get ones digit
+            price = abs(secret) % 10
+            # print(secret, ":", price)
+            secret_prices[og_secret].append(price)
+        # print()
+    print(secret_prices)
+
+    # get the diffs...?
+    secret_diffs = {}
+    for secret in secret_prices:
+        prices = secret_prices[secret]
+        diffs = []
+        for i in range(len(prices)-1):
+            p1 = prices[i]
+            p2 = prices[i+1]
+            diff = p2 - p1
+            diffs.append(diff)
+        secret_diffs[secret] = diffs
+    print(secret_diffs)
+
+    # do a sliding window of size 4 over the first sequence
+    first_secret = secrets[0]
+    first_diffs = secret_diffs[first_secret]
+    first_prices = secret_prices[first_secret]
+
+    window_size = 4
+    for i in range(len(first_diffs) - window_size + 1):
+        changes = first_diffs[i: i + window_size]
+        if changes[0] == -2 and changes[1] == 1 and changes[2] == -1 and changes[3] == 3:
+            first_cost = first_prices[i+window_size]
+            print(changes, first_cost)
+            # try finding the first value in other secret_diffs where that sequence occurs, it could not occur, too
+
+    # sum everything
+    # find the max
+
+    total = 0
+
     print("Total", total)
 
-def part2():
-    lines = parseInput(22)
-    print(lines)
-
 if __name__ == "__main__":
-    print("\nPART 1 RESULT")
-    start = time.perf_counter()
-    part1()
-    end = time.perf_counter()
-    print("Time (ms):", (end - start) * 1000)
-
-    # print("\nPART 2 RESULT")
+    # print("\nPART 1 RESULT")
     # start = time.perf_counter()
-    # part2()
+    # part1()
     # end = time.perf_counter()
     # print("Time (ms):", (end - start) * 1000)
+
+    print("\nPART 2 RESULT")
+    start = time.perf_counter()
+    part2()
+    end = time.perf_counter()
+    print("Time (ms):", (end - start) * 1000)
