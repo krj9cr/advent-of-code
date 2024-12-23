b21e783cbb03d758497ccd3beb2d13185262667c
krj9cr_uva <krj9cr@virginia.edu>
2024-12-23 08:54:03 -0500

    Day 23 pt 1

--- year_2024/src/Day23.py
+++ year_2024/src/Day23.py
@@ -1,18 +1,39 @@
 import time
+import networkx as nx
 
 def parseInput(day):
     dayf = "{:02d}".format(day)
     path = __file__.rstrip(f"Day{dayf}.py") + f"../input/day{dayf}.txt"
     with open(path, 'r') as file:
-        lines = []
+        edges = []
         for line in file:
-            line = line.strip()
-            lines.append(line)
-        return lines
+            line = line.strip().split("-")
+            edges.append(line)
+        nodes = set([item for sublist in edges for item in sublist])
+        G = nx.Graph()
+        G.add_nodes_from(nodes)
+        G.add_edges_from(edges)
+        return G, nodes, edges
+
+def starts_with_t(node):
+    return node.startswith("t")
 
 def part1():
-    lines = parseInput(23)
-    print(lines)
+    G, nodes, edges = parseInput(23)
+    print(G)
+    t_nodes = list(filter(starts_with_t, nodes))
+    print(t_nodes)
+
+    total = 0
+    for clique in nx.enumerate_all_cliques(G):
+        if len(clique) == 3:
+            for node in clique:
+                if node in t_nodes:
+                    print(clique)
+                    total += 1
+                    break
+
+    print(total)
 
 def part2():
     lines = parseInput(23)
