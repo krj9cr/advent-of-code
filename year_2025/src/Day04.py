f2f8db460f9b83b9550ce6f23bd31ff2f5eba8be
krj9cr_uva <krj9cr@virginia.edu>
2025-12-03 19:55:14 -0500

    Prep day 4

---
+++ year_2025/src/Day04.py
@@ -0,0 +1,37 @@
+import time, os
+
+def parseInput():
+    # Get the day number from the current file
+    full_path = __file__
+    file_name = os.path.basename(full_path)
+    dayf = file_name.strip("Day").strip(".py")
+
+    # Find the input file for this day and read in its lines
+    path = __file__.rstrip(f"Day{dayf}.py") + f"../input/day{dayf}.txt"
+    with open(path, 'r') as file:
+        lines = []
+        for line in file:
+            line = line.strip()
+            lines.append(line)
+        return lines
+
+def part1():
+    lines = parseInput()
+    print(lines)
+
+def part2():
+    lines = parseInput()
+    print(lines)
+
+if __name__ == "__main__":
+    print("\nPART 1 RESULT")
+    start = time.perf_counter()
+    part1()
+    end = time.perf_counter()
+    print("Time (ms):", (end - start) * 1000)
+
+    # print("\nPART 2 RESULT")
+    # start = time.perf_counter()
+    # part2()
+    # end = time.perf_counter()
+    # print("Time (ms):", (end - start) * 1000)
